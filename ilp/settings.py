9373e12f82087ccec762c8579f73e795961c35d1
Haris Ibrahim K. V <blucalvin@gmail.com>
2017-06-16 10:57:25 +0530

    Uses the django postgis backend for the Database

--- ilp/settings.py
+++ ilp/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'ilp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'ilpproduction',
         'USER': 'klp',
         'PASSWORD': 'klp',
