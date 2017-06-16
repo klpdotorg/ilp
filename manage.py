b4df2ca7c248b6479bfaf0ddb7a9f4e35e5e9a4c
Haris Ibrahim K. V <blucalvin@gmail.com>
2017-06-16 10:47:31 +0530

    Removes the print statement

--- manage.py
+++ manage.py
@@ -6,8 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ilp.settings")
     sys.path.append(os.path.join(os.getcwd(), 'apps'))
 
-    print>>sys.stderr, sys.path
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
