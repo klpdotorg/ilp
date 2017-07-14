96a41a64505aa935c7f9d2e95b1a4efe167206c4
Shivangi <shivangi@klp.org.in>
2017-07-14 17:11:03 +0530

    Added staff, student, staffstudentrelation, studentstudentrelation, studentgroup import file

--- scripts/institution/updateinstitutiondata.py
+++ scripts/institution/updateinstitutiondata.py
@@ -1,14 +1,19 @@
 from os import system,sys
 import os
 
+if len(sys.argv) != 3:
+    print("Please give database names as arguments. USAGE: python updateinstitutiondata.py dubdubdub ilp", file=sys.stderr)
+    sys.exit()
+
+
 #Before running this script
 #change this to point to the ems database that is used for getting the data
-fromdatabase = "dubdubdub"
+fromdatabase = sys.argv[1]
 
 #change this to ilp db to be populated with
-todatabase = "ilp"
+todatabase = sys.argv[2]
 
-basename = "update"
+basename = "instupdate"
 
 inputdatafile = basename+"_getdata.sql"
 tempdatafile = basename+"_tempdata.sql"
@@ -22,12 +27,16 @@ tables=[
             'columns': 'year_established,rural_urban,village,phone_number,dise_code',
             'query': "COPY(select estdyear,rururb,vilname,mobile1,dise_code from ssa_school, tb_school where ssa_school.schcd=tb_school.dise_code ) TO '$PWD/load/update_ssadata.csv' NULL 'null' DELIMITER   ',' quote '\\\"' csv;",
             'update_query': "UPDATE schools_institution set year_established=temp_info.year_established, rural_urban=temp_info.rural_urban, village=temp_info.village, phone_number=temp_info.phone_number from temp_info where schools_institution.dise_code = temp_info.dise_code;"
+        },
+        {
+            'name': 'update_electedrep',
+            'tablename': 'schools_institution',
+            'temptablename': 'temp_elect',
+            'createcolumns': 'sid integer, ward_id integer, mla_id integer, mp_id integer',
+            'columns': 'sid, ward_id, mla_id, mp_id',
+            'query': "COPY(select sid, ward_id, mla_const_id, mp_const_id from mvw_school_electedrep) TO '$PWD/load/update_electedrep.csv' NULL 'null' DELIMITER ',' quote '\\\"' csv;",
+            'update_query': "UPDATE schools_institution set mp_id=temp.mp_id, mla_id=temp.mla_id, ward_id=temp.ward_id from temp_elect temp where schools_institution.id = temp.sid;"
         }
-        #{
-            #'name': 'update_electedrep',
-            #'tablename': 'schools_institution',
-            #'temptablename': 'temp_elect',
-        #}
 ]
 
 #Create directory and files
