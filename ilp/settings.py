6d6fff5e09dc7d3b6d00ec40fa967ed648105d96
Nabeel Valapra <nabeelvalapra@gmail.com>
2017-07-18 13:43:35 +0530

    fixed syntax error

--- ilp/settings.py
+++ ilp/settings.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
     'KLPLISTVIEW_PAGE_SIZE': 50
 }
 
