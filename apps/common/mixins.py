b1ac354ce76364e24225e5a7228e72fcace4e49d
Nabeel Valapra <nabeelvalapra@gmail.com>
2017-08-23 18:03:46 +0530

    Added test for the above code

--- apps/common/mixins.py
+++ apps/common/mixins.py
@@ -10,6 +10,7 @@ from boundary.models import Boundary, BoundaryHierarchy
 
 logger = logging.getLogger(__name__)
 
+
 class CacheMixin(APIView):
     @classmethod
     def as_view(cls, **initkwargs):
@@ -23,19 +24,15 @@ class CacheMixin(APIView):
 class ILPStateMixin(object):
 
     def get_state(self):
-        state_code =  self.request.query_params.get('state', None)
-        print("State code passed in via args is: ", state_code)
+        state_code = self.request.query_params.get('state', None)
         state_name = STATE_CODES.get(state_code, None)
-        print("State code translates to: ", state_name)
         state = Boundary.objects.get(
             name__iexact=state_name, boundary_type__name='State')
         return state
 
     def get_state_boundaries(self):
         state_code = self.request.query_params.get('state', None)
-        print("State code passed in via args is: ", state_code)
         state_name = STATE_CODES.get(state_code, None)
-        print("State code translates to: ", state_name)
         state = Boundary.objects.get(
             name__iexact=state_name, boundary_type__name='State')
         return BoundaryHierarchy.objects.filter(admin0_id=state.id)
