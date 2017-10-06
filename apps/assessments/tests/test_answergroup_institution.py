1b2ed910db3e63ec79dab74f734a9f43bde152af
Nabeel Valapra <nabeelvalapra@gmail.com>
2017-10-06 15:24:36 +0530

    Completed test case

--- apps/assessments/tests/test_answergroup_institution.py
+++ apps/assessments/tests/test_answergroup_institution.py
@@ -1,17 +1,11 @@
-from mock import patch
-
 from django.urls import reverse
 from django.core.management import call_command
 
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from assessments.models import (
-    AnswerGroup_Institution, RespondentType
-)
-
 from assessments.tests.test_fixtures.meta import (
-    INSTITUTION_ID
+    ANSWERGROUP_INSTITUTION_IDS
 )
 
 
@@ -23,8 +17,12 @@ class AGroupInstitutionViewset(APITestCase):
                      ('apps/assessments/tests/test_fixtures/'
                       'answergroup_institution.json'))
 
-    def test_get(self):
+    def test_answergroup_institution_list(self):
         url = reverse('assessment:answergroup_institution',
-                      kwargs={'survey_id': 6})
+                      kwargs={'survey_id': 7})
         response = self.client.get(url, {'state': 'ka'})
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('Parents', response.data['respondents'])
+        self.assertEqual(response.data['total']['stories'],
+                         len(ANSWERGROUP_INSTITUTION_IDS))
