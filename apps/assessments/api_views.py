d467964419d16a339886da89a8a09193000393dc
Nabeel Valapra <nabeelvalapra@gmail.com>
2017-10-09 17:08:29 +0530

    improved the code

--- apps/assessments/api_views.py
+++ apps/assessments/api_views.py
@@ -18,14 +18,62 @@ from rest_framework.exceptions import APIException
 
 class QGroupAnswerAPIView(ILPListAPIView):
     """
-    Returns total number of stories and schools with stories
+    Returns total number of stories(surverys) and schools with stories
     along with respondent types.
     """
-
-    def get(self, request, survey_id, qgroup_id):
-        # Will be survery_id as kwargs
+    def get(self, request, *args, **kwargs):
+        survey_id = kwargs['survey_id']
+        qgroup_id = kwargs['qgroup_id']
+        admin1_id = self.request.query_params.get('admin1', None)
         source = self.request.query_params.get('source', None)
         versions = self.request.query_params.getlist('version', None)
+        institution_qs, agroup_inst_qs = \
+            self.get_querysets(survey_id, qgroup_id)
+
+        sources = Source.objects.all()
+        if source:
+            sources = sources.filter(name=source)
+        sources = sources.values_list('name', flat=True)
+
+        unique_inst_counts_per_source = agroup_inst_qs.values(
+            'questiongroup__source__name'
+        ).annotate(
+            institution_count=Count('institution', distinct=True)
+        )
+
+        response_source = {}
+        for source in sources:
+            groups = QuestionGroup.objects.filter(source__name=source)
+            source_agroup_inst_qs = agroup_inst_qs.filter(
+                questiongroup__in=groups)
+
+            if versions:
+                versions = map(int, versions)
+                agroup_inst_qs = agroup_inst_qs.filter(
+                    questiongroup__version__in=versions)
+
+            response_source[source] = self.get_json(
+                source, source_agroup_inst_qs, unique_inst_counts_per_source
+            )
+
+        response_json = {}
+        response_json['total'] = {
+            'schools': institution_qs.count(),
+            'stories': agroup_inst_qs.count(),
+            'schools_with_stories':
+                agroup_inst_qs.distinct('institution').count()
+        }
+        response_json['respondents'] = self.get_respondents(agroup_inst_qs)
+        response_json['users'] = self.get_users(agroup_inst_qs)
+        response_json['top_summary'] = \
+            self.get_total_summary(
+                institution_qs.count(), institution_qs, admin1_id
+            )
+        response_json.update(response_source)
+
+        return Response(response_json)
+
+    def get_querysets(self, survey_id, qgroup_id):
         admin1_id = self.request.query_params.get('admin1', None)
         admin2_id = self.request.query_params.get('admin2', None)
         admin3_id = self.request.query_params.get('admin3', None)
@@ -117,50 +165,7 @@ class QGroupAnswerAPIView(ILPListAPIView):
                 date_of_visit__lte=end_date
             )
 
-        sources = Source.objects.all()
-        if source:
-            sources = sources.filter(name=source)
-        sources = sources.values_list('name', flat=True)
-
-        unique_inst_counts_per_source = agroup_inst_qs.values(
-            'questiongroup__source__name'
-        ).annotate(
-            institution_count=Count(
-                'institution', distinct=True
-            )
-        )
-
-        response_source = {}
-        for source in sources:
-            groups = QuestionGroup.objects.filter(source__name=source)
-            source_agroup_inst_qs = agroup_inst_qs.filter(
-                questiongroup__in=groups)
-
-            if versions:
-                versions = map(int, versions)
-                agroup_inst_qs = agroup_inst_qs.filter(
-                    questiongroup__version__in=versions)
-
-            response_source[source] = self.get_json(
-                source, source_agroup_inst_qs, unique_inst_counts_per_source
-            )
-
-        response_json = {}
-        response_json['total'] = {
-            'schools': institution_qs.count(),
-            'stories': agroup_inst_qs.count(),
-            'schools_with_stories':
-                agroup_inst_qs.distinct('institution').count()
-        }
-        response_json['respondents'] = self.get_respondents(agroup_inst_qs)
-        response_json['users'] = self.get_users(agroup_inst_qs)
-        response_json['top_summary'] = \
-            self.get_total_summary(
-                institution_qs.count(), institution_qs, admin1_id
-            )
-        response_json.update(response_source)
-
-        return Response(response_json)
+        return (institution_qs, agroup_inst_qs)
 
     def get_total_summary(self, inst_count, inst_qs, admin1_id):
         pass
