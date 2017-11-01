f933db57f3530d839895e4aa94f350c583a181d6
Shivangi <shivangi@klp.org.in>
2017-11-01 22:10:25 +0530

    Added materialized views for boundary

---
+++ imports/aggregates/refresh_materialized_views.sql
@@ -0,0 +1,19 @@
+REFRESH MATERIALIZED VIEW mvw_institution_aggregations;
+
+REFRESH MATERIALIZED VIEW mvw_institution_stu_gender_count;
+
+REFRESH MATERIALIZED VIEW mvw_institution_class_year_stucount;
+
+REFRESH MATERIALIZED VIEW mvw_boundary_hierarchy;
+    
+REFRESH MATERIALIZED VIEW mvw_boundary_basic_agg;
+
+REFRESH MATERIALIZED VIEW mvw_boundary_school_gender_agg;
+
+REFRESH MATERIALIZED VIEW mvw_boundary_school_category_agg;
+
+REFRESH MATERIALIZED VIEW mvw_boundary_school_mgmt_agg;
+
+REFRESH MATERIALIZED VIEW mvw_boundary_student_mt_agg;
+
+REFRESH MATERIALIZED VIEW mvw_boundary_school_moi_agg;
