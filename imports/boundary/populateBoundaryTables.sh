3c691d9866cc7f35adcc98e2e55d898ffd4e2518
shivangi <shivangi@klp.org.in>
2017-11-07 09:40:55 +0530

    adding main import script

--- imports/boundary/populateBoundaryTables.sh
+++ imports/boundary/populateBoundaryTables.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Call this script with the database name
 if [ $# -eq 0 ]; then
-    echo "Please supply database names as argument. USAGE: `basename $0` klpdbname electedrepdbname newdatabasename pathtostorecsvs"
+    echo "Please supply database names as argument. USAGE: `basename $0` klpdbname electedrepdbname newdatabasename pathtostorecsvs pathtosqls"
     exit 1;
 fi
 echo "######################"
@@ -11,18 +11,21 @@ legacydb="$1";
 ilpdbname="$3";
 electedrepdbname="$2"
 csvdirname="$4";
+sqldir="$5";
+
+mkdir -p $csvdirname
 
 #Delete from tables
-psql -U klp -d $ilpdbname -f sql/deleteFromTables.sql
+psql -U klp -d $ilpdbname -f $sqldir/deleteFromTables.sql
 
 #Add entries for India and Karnataka in the boundary table. Need to add more states here as we go along
-psql -U klp -d $ilpdbname -f sql/fillBoundaryStatics.sql
+psql -U klp -d $ilpdbname -f $sqldir/fillBoundaryStatics.sql
 
 #Export the boundary table data
-psql -U klp -d $legacydb --set=outputdir="$csvdirname" -f sql/exportBoundaryData.sql
+psql -U klp -d $legacydb --set=outputdir="$csvdirname" -f $sqldir/exportBoundaryData.sql
 
 #Import the boundary table data
-psql -U klp -d $ilpdbname --set=inputdir="$csvdirname" -f sql/importBoundaryTable.sql
+psql -U klp -d $ilpdbname --set=inputdir="$csvdirname" -f $sqldir/importBoundaryTable.sql
 exit_status=$?
 if [ $exit_status -eq 1 ]; then
     echo "SQL script execution failed with error";
@@ -38,10 +41,10 @@ if [ $exit_status -eq 1 ]; then
 fi
 
 #Export the election boundary data from electrep_new DB
-psql -U klp -d $electedrepdbname --set=outputdir="$csvdirname" -f sql/exportElectionBoundary.sql
+psql -U klp -d $electedrepdbname --set=outputdir="$csvdirname" -f $sqldir/exportElectionBoundary.sql
 
 #Import the election boundary data from the CSV into boundary_electionboundary table
-psql -U klp -d $ilpdbname --set=inputdir="$csvdirname" -f sql/importElectionBoundary.sql
+psql -U klp -d $ilpdbname --set=inputdir="$csvdirname" -f $sqldir/importElectionBoundary.sql
 exit_status=$?
 if [ $exit_status -eq 1 ]; then
     echo "SQL script execution failed with error";
