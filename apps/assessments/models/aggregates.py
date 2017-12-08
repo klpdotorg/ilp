0f3a05f8129fed14ab3920b61f9bcc209ec3160a
shivangi <shivangi@klp.org.in>
2017-12-08 10:24:53 +0530

    corrected field name in assessment aggregates

--- apps/assessments/models/aggregates.py
+++ apps/assessments/models/aggregates.py
@@ -171,7 +171,7 @@ class SurveyClassGenderAgg(models.Model):
     sg_name = models.CharField(max_length=100, db_column="sg_name")
     gender = models.ForeignKey("common.Gender", db_column="gender")
     num_assessments = models.IntegerField(db_column="num_assessments")
-    num_perfectscore_assessments = models.IntegerField(db_column="num_perfectscore_assessments")
+    num_correct_assessments = models.IntegerField(db_column="num_correct_assessments")
 
     class Meta:
         managed = False
