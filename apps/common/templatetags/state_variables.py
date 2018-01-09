0003ec6dfa205a7fde0e3a58226377c6dee0a9d3
Ansal S <ansal@bssatech.com>
2018-01-09 08:33:57 +0530

    move api.js out of gka branch

--- apps/common/templatetags/state_variables.py
+++ apps/common/templatetags/state_variables.py
@@ -6,6 +6,7 @@ register = template.Library()
 
 
 # States and values
+# state_code is used in JS for state param in AJAX requests
 
 STATE = {
     'ilp': {
@@ -16,7 +17,8 @@ STATE = {
         'site_url': 'https://ilp.org.in',
         'home_logo': '',
         'header_logo': '',
-        'footer_logo': ''
+        'footer_logo': '',
+        'state_code': ''
     },
     'ka': {
         'name': 'Karnataka',
@@ -26,7 +28,8 @@ STATE = {
         'site_url': 'https://klp.org.in',
         'home_logo': '/static/images/home_klp_logo@2x.png',
         'header_logo': '/static/images/KLP_logo@2x.png',
-        'footer_logo': '/static/images/KLP_logo_footer@2x.png'
+        'footer_logo': '/static/images/KLP_logo_footer@2x.png',
+        'state_code': 'ka'
     },
     'od': {
         'name': 'Odisha',
@@ -36,7 +39,8 @@ STATE = {
         'site_url': 'https://olp.org.in',
         'home_logo': '/static/images/states/od/home_olp_logo@2x.png',
         'header_logo': '/static/images/states/od/OLP_logo@2x.png',
-        'footer_logo': '/static/images/states/od/OLP_logo_footer@2x.png'
+        'footer_logo': '/static/images/states/od/OLP_logo_footer@2x.png',
+        'state_code': 'od'
     },
 }
 
