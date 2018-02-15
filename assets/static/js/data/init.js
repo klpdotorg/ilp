5c211fe63ef1eb153d3e8186b74594d9502656ff
awesomepankaj <pankajdcoder@gmail.com>
2018-02-15 23:36:23 +0530

    fix: require login on data download

---
+++ assets/static/js/data/init.js
@@ -0,0 +1,31 @@
+'use strict';
+(function() {
+    klp.init = function() {
+      klp.router = new KLPRouter();
+      klp.router.init();
+
+      function downloadKLPSchools() {
+        window.open(`/media/school-data/primaryschool.csv`, '_self');
+      }
+
+      function downloadKLPPreschools() {
+        window.open('/media/school-data/preschool.csv', '_self');
+      }
+
+      function downloadKLPDISESchools() {
+        window.open('/media/school-data/dise-1314.csv', '_self');
+      }
+
+      $("#download-schools-button").click(function(){
+        klp.auth.requireLogin(downloadKLPSchools);
+      });
+
+      $("#download-preschools-button").click(function() {
+        klp.auth.requireLogin(downloadKLPPreschools);
+      })
+
+      $("#download-diseschools-button").click(function() {
+        klp.auth.requireLogin(downloadKLPDISESchools);
+      })
+    }
+})();
