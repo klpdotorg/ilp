ab07ca636e4b23ea581ce65813cb7acdcdb64a64
Nabeel Valapra <nabeelvalapra@gmail.com>
2018-02-22 13:35:08 +0530

    added the state filter to detail boundary endpoint

--- apps/assessments/api_views/aggregates.py
+++ apps/assessments/api_views/aggregates.py
@@ -654,10 +654,9 @@ class SurveyDetailEBoundaryAPIView(ListAPIView, ILPStateMixin):
     queryset = SurveyBoundaryElectionTypeCount.objects.all()
 
     def list(self, request, *args, **kwargs):
-        boundary_id = self.request.GET.get('boundary_id', None)
         queryset = self.filter_queryset(self.get_queryset())
-        if boundary_id:
-            queryset = queryset.filter(boundary_id=boundary_id)
+        if self.request.GET.get('state', None):
+            queryset = queryset.filter(boundary_id=self.get_state().id)
 
         electioncount_agg = queryset.values('const_ward_type').annotate(
             Sum('electionboundary_count'))
