cbafd6a393e0211acf88ffd56e1bebbbac265cf3
shivangi <shivangi@klp.org.in>
2018-03-05 19:26:58 +0530

    Logic for missed user id due to multiple mobile nos

--- imports/users/update_assessment_userid.py
+++ imports/users/update_assessment_userid.py
@@ -18,16 +18,32 @@ loadsqlfile = scriptdir+"/"+basename+"_loaddata.sql"
 
 tables = [
         {
-            'name': 'assessments_answergroup_institution',
-            'getquery': "\COPY (select id, user_id,group_id from stories_story where user_id is not null) TO 'replacefilename' NULL 'null' DELIMITER   ',' quote '\\\"' csv;",
+            'table_name': 'assessments_answergroup_institution',
+            'name': 'assessments_answergroup_institution_1',
+            'getquery': "\COPY (select id, user_id,group_id from stories_story where user_id is not null and user_id in (select id from users_user where mobile_no not like '%,%')) TO 'replacefilename' NULL 'null' DELIMITER   ',' quote '\\\"' csv;",
             'tempquery': "CREATE TEMP TABLE temp_replacetablename(id integer, user_id integer, group_id integer); \COPY temp_replacetablename(id, user_id, group_id) FROM 'replacefilename' with csv NULL 'null';",
-            'updatequery': "UPDATE replacetablename set created_by_id=temp.user_id from  temp_replacetablename temp, users_user users where replacetablename.id=temp.id and temp.user_id = users.id and replacetablename.questiongroup_id=temp.group_id;"
+            'updatequery': "UPDATE replacetablename set created_by_id=users.id from  temp_replacetablename temp, users_user users where replacetablename.id=temp.id and temp.user_id = users.id and replacetablename.questiongroup_id=temp.group_id;"
         },
         {
-            'name': 'assessments_answergroup_student',
-            'getquery': "\COPY (select id, user_id, group_id from stories_story where user_id is not null) TO 'replacefilename' NULL 'null' DELIMITER   ',' quote '\\\"' csv;",
+            'table_name': 'assessments_answergroup_institution',
+            'name': 'assessments_answergroup_institution_2',
+            'getquery': "\COPY (select story.id, story.user_id, story.group_id, split_part(users.mobile_no,',',1) from stories_story story, users_user users where story.user_id = users.id and users.mobile_no like '%,%') TO 'replacefilename' NULL 'null' DELIMITER   ',' quote '\\\"' csv;",
+            'tempquery': "CREATE TEMP TABLE temp_replacetablename(id integer, user_id integer, group_id integer, mobile_no text); \COPY temp_replacetablename(id, user_id, group_id, mobile_no) FROM 'replacefilename' with csv NULL 'null';",
+            'updatequery': "UPDATE replacetablename set created_by_id=users.id from  temp_replacetablename temp, users_user users where replacetablename.id=temp.id and users.mobile_no = temp.mobile_no and replacetablename.questiongroup_id=temp.group_id;"
+        },
+        {
+            'table_name': 'assessments_answergroup_student',
+            'name': 'assessments_answergroup_student_1',
+            'getquery': "\COPY (select id, user_id, group_id from stories_story where user_id is not null and user_id in (select id from users_user where mobile_no not like '%,%')) TO 'replacefilename' NULL 'null' DELIMITER   ',' quote '\\\"' csv;",
             'tempquery': "CREATE TEMP TABLE temp_replacetablename(id integer, user_id integer, group_id integer); \COPY temp_replacetablename(id, user_id, group_id) FROM 'replacefilename' with csv NULL 'null';",
-            'updatequery': "UPDATE replacetablename set created_by_id=temp.user_id from  temp_replacetablename temp, users_user users where replacetablename.id=temp.id and temp.user_id = users.id and replacetablename.questiongroup_id = temp.group_id;"
+            'updatequery': "UPDATE replacetablename set created_by_id=users.id from  temp_replacetablename temp, users_user users where replacetablename.id=temp.id and temp.user_id = users.id and replacetablename.questiongroup_id = temp.group_id;"
+        },
+        {
+            'table_name': 'assessments_answergroup_student',
+            'name': 'assessments_answergroup_student_2',
+            'getquery': "\COPY (select story.id, story.user_id, story.group_id, split_part(users.mobile_no,',',1) from stories_story story, users_user users where story.user_id = users.id and users.mobile_no like '%,%') TO 'replacefilename' NULL 'null' DELIMITER   ',' quote '\\\"' csv;",
+            'tempquery': "CREATE TEMP TABLE temp_replacetablename(id integer, user_id integer, group_id integer, mobile_no text); \COPY temp_replacetablename(id, user_id, group_id, mobile_no) FROM 'replacefilename' with csv NULL 'null';",
+            'updatequery': "UPDATE replacetablename set created_by_id=users.id from  temp_replacetablename temp, users_user users where replacetablename.id=temp.id and users.mobile_no = temp.mobile_no and replacetablename.questiongroup_id=temp.group_id;"
         }
 ]
 
@@ -50,13 +66,13 @@ def create_sqlfiles():
         open(filename, 'wb', 0)
         os.chmod(filename, 0o666)
         if 'getquery' in table:
-            command = 'echo "'+table['getquery'].replace('replacetablename', table['name']).replace('replacefilename', filename)+'">>'+inputsqlfile
+            command = 'echo "'+table['getquery'].replace('temp_replacetablename', table['name']).replace('replacefilename', filename).replace('replacetablename', table['table_name'])+'">>'+inputsqlfile
             system(command)
         if 'tempquery' in table:
-            command = 'echo "'+table['tempquery'].replace('replacetablename', table['name']).replace('replacefilename', filename)+'">>'+loadsqlfile
+            command = 'echo "'+table['tempquery'].replace('temp_replacetablename', table['name']).replace('replacefilename', filename).replace('replacetablename', table['table_name'])+'">>'+loadsqlfile
             system(command)
         if 'updatequery' in table:
-            command = 'echo "'+table['updatequery'].replace('replacetablename', table['name']).replace('replacefilename', filename)+'">>'+loadsqlfile
+            command = 'echo "'+table['updatequery'].replace('temp_replacetablename', table['name']).replace('replacefilename', filename).replace('replacetablename', table['table_name'])+'">>'+loadsqlfile
             system(command)
 
 
