b1955c35be82f2b0f19c58b422b70a58ac794c05
Ansal S <ansal@bssatech.com>
2018-03-05 22:32:44 +0530

    accordion gka - fix undefined error when section collapses before loading entire data

--- assets/static/js/common/accordion.js
+++ assets/static/js/common/accordion.js
@@ -7,7 +7,7 @@
             $(this).closest('li').find('.content').not(':animated').slideToggle();
             //Update the chartist charts in this accordian section
             $(this).closest('li').find('.ct-chart').each(function(i, e) {
-                e.__chartist__.update();
+                if(e && e.__chartist__) { e.__chartist__.update(); }
             });
         });
     };
