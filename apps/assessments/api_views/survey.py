3128b3829c5e5c25b9373983839fae4f0a0770fd
Nabeel Valapra <nabeelvalapra@gmail.com>
2018-03-07 14:37:42 +0530

    deletes(status=DL) works for boundary/surveys

--- apps/assessments/api_views/survey.py
+++ apps/assessments/api_views/survey.py
@@ -44,7 +44,7 @@ from assessments.models import (
     SurveyBoundaryUserTypeAgg, SurveyBoundaryElectionTypeCount,
     SurveyTagInstitutionMapping
 )
-from common.models import RespondentType
+from common.models import RespondentType, Status
 from assessments.serializers import (
     SurveySerializer, RespondentTypeSerializer,
     SurveyCreateSerializer
@@ -60,13 +60,17 @@ class SurveysViewSet(ILPViewSet, ILPStateMixin):
     filter_class = SurveyTagFilter
 
     def get_serializer_class(self):
-        if self.request.method == 'POST':
+        if self.request.method in ['POST', ]:
             return SurveyCreateSerializer
         return SurveySerializer
 
     def get_queryset(self):
         state = self.get_state()
         return self.queryset.filter(admin0=state)
+
+    def perform_destroy(self, instance):
+        instance.status_id = Status.DELETED
+        instance.save()
 
 
 class SurveyInstitutionDetailAPIView(ListAPIView, ILPStateMixin):
