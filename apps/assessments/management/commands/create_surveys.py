4d0e80f89eb7b2255f4bd0e3ed5d63e8a011e2e7
shivangi <shivangi@klp.org.in>
2018-03-16 18:52:53 +0530

    Added Orissa Teacher Survey

--- apps/assessments/management/commands/create_surveys.py
+++ apps/assessments/management/commands/create_surveys.py
@@ -46,21 +46,23 @@ class Command(BaseCommand):
                 count += 1
                 continue
             count += 1
-            name = row[0].strip()
-            description = row[1].strip()
-            partner = Partner.objects.get(char_id=row[2].strip())
-            status = Status.objects.get(char_id = row[3].strip())
-            state = row[4].strip()
-            survey_on_id = SurveyOnType.objects.get(char_id = row[5].strip())
-            lang_name = row[6].strip()
+            id = row[0].strip()
+            name = row[1].strip()
+            description = row[2].strip()
+            partner = Partner.objects.get(char_id=row[3].strip())
+            status = Status.objects.get(char_id = row[4].strip())
+            state = row[5].strip()
+            survey_on_id = SurveyOnType.objects.get(char_id = row[6].strip())
+            lang_name = row[7].strip()
             survey = Survey.objects.create(
-                              name = name,
-                              description = description,
-                              partner = partner,
-                              status = status,
-                              survey_on_id = 'institution',
-                              lang_name = lang_name,
-                              admin0_id = 2)
+                         id = id,
+                         name = name,
+                         description = description,
+                         partner = partner,
+                         status = status,
+                         survey_on_id = 'institution',
+                         lang_name = lang_name,
+                         admin0_id = 2)
             return survey
 
 
@@ -72,37 +74,39 @@ class Command(BaseCommand):
                 continue
             count += 1
 
-            name = row[0].strip()
-            group_text = row[1].strip()
-            start_date = datetime.strptime(row[2].strip(), "%d-%m-%Y").date()
-            version = row[4].strip()
-            double_entry = row[5].strip()
-            academicyear = AcademicYear.objects.get(char_id=row[6].strip())
-            institution_type = InstitutionType.objects.get(char_id=row[7].strip())
-            source = Source.objects.get(id=row[8].strip())
-            status = Status.objects.get(char_id=row[9].strip())
-            type_id = SurveyType.objects.get(char_id=row[10].strip())
-            description = row[11].strip()
-            image_required = row[12].strip()
-            lang_name = row[13].strip()
-            comments_required = row[14].strip()
+            id = row[0].strip()
+            name = row[1].strip()
+            group_text = row[2].strip()
+            start_date = datetime.strptime(row[3].strip(), "%d-%m-%Y").date()
+            version = row[5].strip()
+            double_entry = row[6].strip()
+            academicyear = AcademicYear.objects.get(char_id=row[7].strip())
+            institution_type = InstitutionType.objects.get(char_id=row[8].strip())
+            source = Source.objects.get(id=row[9].strip())
+            status = Status.objects.get(char_id=row[10].strip())
+            type_id = SurveyType.objects.get(char_id=row[11].strip())
+            description = row[12].strip()
+            image_required = row[13].strip()
+            lang_name = row[14].strip()
+            comments_required = row[15].strip()
             questiongroup = QuestionGroup.objects.create(
-                              name = name,
-                              group_text = group_text,
-                              start_date = start_date,
-                              version = version,
-                              double_entry = double_entry,
-                              created_at = timezone.now(),
-                              academic_year = academicyear,
-                              inst_type = institution_type,
-                              source = source,
-                              status = status,
-                              survey = survey,
-                              type = type_id,
-                              description = description,
-                              lang_name = lang_name,
-                              image_required = image_required,
-                              comments_required = comments_required)
+                                id = id,
+                                name = name,
+                                group_text = group_text,
+                                start_date = start_date,
+                                version = version,
+                                double_entry = double_entry,
+                                created_at = timezone.now(),
+                                academic_year = academicyear,
+                                inst_type = institution_type,
+                                source = source,
+                                status = status,
+                                survey = survey,
+                                type = type_id,
+                                description = description,
+                                lang_name = lang_name,
+                                image_required = image_required,
+                                comments_required = comments_required)
             return questiongroup 
 
 
@@ -115,39 +119,44 @@ class Command(BaseCommand):
                 continue
             count += 1
 
-            question_text = row[0].strip()
-            display_text = row[1].strip()
-            key = self.check_value(row[2].strip())
-            options = self.check_value(row[3].strip())
-            is_featured = self.check_value(row[4].strip(), True)
-            question_type = QuestionType.objects.get(id = self.check_value(row[5].strip(),4))
-            status = Status.objects.get(char_id=row[6].strip())
-            max_score = self.check_value(row[7].strip(),0)
-            pass_score = self.check_value(row[8].strip(),0)
-            lang_name = self.check_value(row[9].strip())
-            lang_options = self.check_value(row[10].strip())
+            id = row[0].strip()
+            question_text = row[1].strip()
+            display_text = row[2].strip()
+            key = self.check_value(row[3].strip())
+            options = self.check_value(row[4].strip())
+            is_featured = self.check_value(row[5].strip(), True)
+            question_type = QuestionType.objects.get(id = self.check_value(row[6].strip(),4))
+            status = Status.objects.get(char_id=row[7].strip())
+            max_score = self.check_value(row[8].strip(),0)
+            pass_score = self.check_value(row[9].strip(),0)
+            lang_name = self.check_value(row[10].strip())
+            lang_options = self.check_value(row[11].strip())
             question = Question.objects.create(
-                              question_text = question_text,
-                              display_text = display_text,
-                              key = key,
-                              options = options,
-                              is_featured = is_featured,
-                              question_type = question_type,
-                              status = status,
-                              max_score = max_score,
-                              pass_score = pass_score,
-                              lang_name = lang_name,
-                              lang_options = lang_options)
+                            id = id,
+                            question_text = question_text,
+                            display_text = display_text,
+                            key = key,
+                            options = options,
+                            is_featured = is_featured,
+                            question_type = question_type,
+                            status = status,
+                            max_score = max_score,
+                            pass_score = pass_score,
+                            lang_name = lang_name,
+                            lang_options = lang_options)
             questions.append(question)
         return questions
 
 
     def map_questiongroup_questions(self, questiongroup, questions):
         qgq_maps = []
+        sequence = 1
         for question in questions:
             qgq_map = QuestionGroup_Questions.objects.create(
+                      sequence = sequence,
                       question = question,
                       questiongroup = questiongroup)
+            sequence += 1
             qgq_maps.append(qgq_map)
 
         return qgq_maps
