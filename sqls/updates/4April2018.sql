283b4c5ac80eddec41d3774ec6e622e8fc51c7d2
shivangi <shivangi@klp.org.in>
2018-04-09 11:07:54 +0530

    All updates documented, added update for default respondent type

---
+++ sqls/updates/4April2018.sql
@@ -0,0 +1,5 @@
+update assessments_questiongroupkey set key='Fractions' where key='Fraction';
+update assessments_questiongroupkey set key='Decimals' where key='Decimal';
+update assessments_questiongroupkey set key='Area of Shapes' where key='Area of shape';
+update assessments_questiongroupkey set key='Word Problems' where key='Word Problem';
+
