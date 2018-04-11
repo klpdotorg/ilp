42d16e2008e2f16defaa53f88e80bf6b53b4b0e4
shivangi <shivangi@klp.org.in>
2018-04-11 17:37:51 +0530

    Adding 35 and 37 questiongroups for key Deimcals

---
+++ sqls/updates/11April2018.sql
@@ -0,0 +1,2 @@
+insert into assessments_questiongroupkey(key, max_score, questiongroup_id) values('Decimal', 1, 35);
+insert into assessments_questiongroupkey(key, max_score, questiongroup_id) values('Decimal', 1, 37);
