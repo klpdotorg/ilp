90d12a4d8ec72872a3b9bf6d4e7816b78ff0c8bd
shivangi <shivangi@klp.org.in>
2018-04-12 18:47:12 +0530

    Decimal to Decimals

--- sqls/updates/11April2018.sql
+++ sqls/updates/11April2018.sql
@@ -1,2 +1,2 @@
-insert into assessments_questiongroupkey(key, max_score, questiongroup_id) values('Decimal', 1, 35);
-insert into assessments_questiongroupkey(key, max_score, questiongroup_id) values('Decimal', 1, 37);
+insert into assessments_questiongroupkey(key, max_score, questiongroup_id) values('Decimals', 1, 35);
+insert into assessments_questiongroupkey(key, max_score, questiongroup_id) values('Decimals', 1, 37);
