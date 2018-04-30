8eec2ea6672f356d7aefd0619dcedb66ac8e0ea2
brijeshpoojary <brijesh@klp.org.in>
2018-04-30 14:59:07 +0530

    gp contest and update sqls

---
+++ sqls/updates/30April2018/updates.sql
@@ -0,0 +1,2 @@
+update assessments_answergroup_institution set institution_id=4302 where institution_id=2302 and questiongroup_id in (21,22,23);
+update assessments_answergroup_institution set institution_id=3078 where institution_id=39263 and questiongroup_id in (21,22,23);
