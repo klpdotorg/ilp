c446bd2a89a62a98d2271ac723b0b32437f86bd0
Hasna <hasna@hamon.in>
2018-05-04 12:26:03 +0530

    Add math contest report template

--- apps/reports/tests.py
+++ apps/reports/tests.py
@@ -1,3 +1,26 @@
+from io import StringIO
+from django.core.management import call_command
 from django.test import TestCase
+import tempfile
+import os.path
+from reports.reports import ReportOne 
 
-# Create your tests here.
+#class ReportGenerateTest(TestCase):
+    # def test_command_output(self):
+    #     with open('/tmp/testout', 'w') as out_file:
+    #         #StringIO()
+    #         # args = ('sndfs', 'adfadf', 'report_one','pdf', 'test')
+    #         call_command('reportgen','sndfs', 'adfadf', 'report_one','pdf', 'test', stdout=out_file)
+    #         self.assertIn('success', out_file.read())
+    
+            
+class GeneratedOutputTest(TestCase):
+    def test_html_output(self):
+        r = ReportOne('from_date','to_date')
+        r.get_html('test_run')
+        self.assertTrue(os.path.exists('apps/reports/output/test_run.html'))
+
+    def test_pdf_output(self):
+        r = ReportOne('from_date','to_date')
+        r.get_pdf('test_run')
+        self.assertTrue(os.path.exists('apps/reports/reports_pdf/test_run.pdf'))
