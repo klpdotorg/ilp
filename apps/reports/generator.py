48a778452586bc3d72f307eb14926a38fc882882
Hasna <hasna@hamon.in>
2018-05-10 12:20:21 +0530

    Add wrapper function for genarate report

--- apps/reports/generator.py
+++ apps/reports/generator.py
@@ -1,17 +1,27 @@
 import django_filters
-from reports.reports import ReportOne, ReportTwo,GPMathContestReport
+from reports.reports import ReportOne,GPMathContestReport
 
-reportlist = {"report_one":ReportOne,"report_two": ReportTwo,"gp_contest_report": GPMathContestReport}
+reportlist = {"report_one":ReportOne, "gp_contest_report": GPMathContestReport}
 
-def generate_report(report_format, report_type, output_name, *args):
+
+
+def generate_report_internal(report_format, report_type, output_name, *args):
     try:
         r = reportlist[report_type]
         report = r(*args)
         report.generate(report_format, output_name)
-        report.save()
-        print('success')
+        result = report.save()
+        return result.id
     except KeyError:
-        print("Invalid report type")
-
+        sys.stderr.write("{} is not a valid report type".format(report_type))
+        raise
     except ValueError :
-        print("Error in Report generation")
+        sys.stderr.write("Error in Report generation")
+        raise
+
+def generate_report(report_format, report_type, output_name, *args):
+    try:
+        rid = generate_report_internal(report_format, report_type, output_name, *args)
+        print ("Report created. Id is {}".format(rid))
+    except (KeyError, ValueError) as e:
+        sys.exit(-2)
