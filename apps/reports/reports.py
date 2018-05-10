f3c506aac0eec7ea78ea3d47e2251053bd5fb84f
Hasna <hasna@hamon.in>
2018-05-10 16:38:43 +0530

    Add preformance to report template

--- apps/reports/reports.py
+++ apps/reports/reports.py
@@ -126,6 +126,10 @@ class GPMathContestReport(BaseReport):
                 schools.append(details)
 
         #Calculate the perfomance of students
+        boys_100 = 0
+        girls_100 = 0
+        boys_zero = 0
+        girls_zero = 0
         for i in scores.values():
             total = sum(i['mark'])/len(i['mark'])
             if total == 100.0:
@@ -165,7 +169,7 @@ class GPMathContestReport(BaseReport):
                         if not gradeExist:
                             o['grades'].append({'name':item['grade'],'values':[{'contest':item['contest'],'count':item['percent']}]})
 
-        return {'gp_name': gp, 'academic_year': ay, 'block':block, 'district':district,'no_schools_gp':gp_schools,'no_students':number_of_students,'today':report_generated_on,'boys':num_boys,'girls':num_girls,'schools':out,'cs':contest_list}
+        return {'gp_name': gp, 'academic_year': ay, 'block':block, 'district':district,'no_schools_gp':gp_schools,'no_students':number_of_students,'today':report_generated_on,'boys':num_boys,'girls':num_girls,'schools':out,'cs':contest_list,'score_100':score_100,'score_zero':score_zero,'girls_zero':girls_zero,'boys_zero':boys_zero,'boys_100':boys_100,'girls_100':girls_100}
 
 
 
