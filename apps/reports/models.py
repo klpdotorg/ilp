c0d4a6e0a5c0a35c1f9353bfc52dd01fbfa21ee7
Hasna <hasna@hamon.in>
2018-05-16 11:37:27 +0530

    Create new table for report Tracking. Add field link_id to reports

--- apps/reports/models.py
+++ apps/reports/models.py
@@ -4,5 +4,15 @@ from django.contrib.postgres.fields import JSONField
 # Create your models here.
 class Reports(models.Model):
     report_type = models.CharField(max_length=50)
+    link_id = models.CharField(max_length=10,unique=True,null=True)
     parameters = JSONField()
+
+
+class Tracking(models.Model):
+    report_id = models.ForeignKey('Reports', db_column="link_id")
+    track_id = models.CharField(max_length=10)
+    visit_count = models.IntegerField()
+    download_count = models.IntegerField()
+    visited_at = models.DateField()
+    downloaded_at = models.DateField()
     
