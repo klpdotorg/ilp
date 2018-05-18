92ff4c6e1e042786ae4ffda61d9367fea8400323
Hasna <hasna@hamon.in>
2018-05-18 10:43:23 +0530

    Add gp_name argument in send_reports command

--- apps/reports/links.py
+++ apps/reports/links.py
@@ -9,17 +9,20 @@ from .reportlist import reportlist
 from .contacts import contacts
 from common.utils import send_sms
 
-def send_link(dry, filepath):
+def send_link(dry, filepath, gp_name):
     print('start')
-    frequency_str = '1,16,17'
+    frequency_str = '1,16,17,18'
     r_type = 'GPMathContestReport'
-    params = {'gp_name': 'abbinahole', 'academic_year': '2017-2018'}
+    params = {'gp_name': gp_name, 'academic_year': '2017-2018'}
 
     frequency = frequency_str.split(',')
     today = datetime.datetime.now().strftime("%d")
 
+    report_status = True
     for d in frequency:
         if d==today:
+            print('Scheduled {} for {} panchayat sending now'.format(r_type,gp_name))
+            report_status = False
             r = reportlist[r_type]
             report = r()
             report.params = params
@@ -35,3 +38,5 @@ def send_link(dry, filepath):
                     else:
                         print(person[1],sms)
                         # send_sms(person['number'],sms)
+    if report_status:
+        print("No reports scheduled today")
--- apps/reports/management/commands/send_reports.py
+++ apps/reports/management/commands/send_reports.py
@@ -13,17 +13,23 @@ class Command(BaseCommand):
         )
         parser.add_argument(
             '--filename',
-            help='Dry run of command instead of actual run',
+            help='file name of contacts in csv format',
+        )
+        parser.add_argument(
+            '--gp_name',
+            help='gram panchayat name for report',
         )
 
     def handle(self, *args, **options):
         dry = False
         filename = 'contacts.csv'
-
+        gp_name = 'abbinahole'
         if options['filename']:
             filename =  options['filename']
         if options['dry']:
             print('dry running')
             dry = True
+        if options['gp_name']:
+           gp_name = options['gp_name']
 
-        send_link(dry, filename)
+        send_link(dry, filename, gp_name)
