18b96471105211abd8b19d93e3ac4265f05ac812
Suroor Hussain <suroorhussain@hamon.in>
2018-05-29 12:37:25 +0530

    Calculate the gender-wise perfomance of students

--- apps/reports/reports.py
+++ apps/reports/reports.py
@@ -317,6 +317,26 @@ class SchoolReport(BaseReport):
                 details['percent'] = sum(percent)/len(percent)
                 schools.append(details)
 
+        #Calculate the perfomance of students
+        boys_100 = 0
+        girls_100 = 0
+        boys_zero = 0
+        girls_zero = 0
+        for i in scores.values():
+            total = sum(i['mark'])/len(i['mark'])
+            if total == 100.0:
+                if i['gender'] == 'Male':
+                    boys_100 += 1
+                else:
+                    girls_100 += 1
+            elif total == 0.0:
+                if i['gender'] == 'Male':
+                    boys_zero += 1
+                else:
+                    girls_zero += 1
+
+        score_100 = boys_100 + girls_100
+        score_zero = boys_zero + girls_zero
 if __name__ == "__main__":
     r= ReportOne();
     r.get_data
