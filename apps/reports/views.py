a69fa1b09abae7a64d52a194114caf8bf0a5cf6c
Suroor Hussain <suroorhussain@hamon.in>
2018-06-01 15:29:46 +0530

    Bugfix: Not able to view report without tracking id

--- apps/reports/views.py
+++ apps/reports/views.py
@@ -11,7 +11,7 @@ def view_report(request, report_id, tracking_id='default'):
     report = get_object_or_404(Reports, link_id=report_id)
     data = report.data
 
-    tracker = Tracking.objects.get(track_id=tracking_id)
+    tracker = Tracking.objects.get(track_id=tracking_id, report_id__link_id=report_id)
     tracker.visit_count += 1
     tracker.visited_at = datetime.datetime.now()
     tracker.save()
@@ -24,7 +24,7 @@ def download_report(request, report_id, tracking_id='default'):
     pdf = report.get_pdf()
     filename = report_model.report_type+datetime.datetime.now().strftime("%d%m%y")+'.pdf'
 
-    tracker = Tracking.objects.get(track_id=tracking_id)
+    tracker = Tracking.objects.get(track_id=tracking_id, report_id__link_id=tracking_id)
     tracker.download_count += 1
     tracker.downloaded_at = datetime.datetime.now()
     tracker.save()
