66d34f857612f3f0e9fb46f0029e6a67425efff7
Suroor Hussain <suroorhussain@hamon.in>
2018-06-01 15:38:12 +0530

    Fix typo in variable name

--- apps/reports/views.py
+++ apps/reports/views.py
@@ -24,7 +24,7 @@ def download_report(request, report_id, tracking_id='default'):
     pdf = report.get_pdf()
     filename = report_model.report_type+datetime.datetime.now().strftime("%d%m%y")+'.pdf'
 
-    tracker = Tracking.objects.get(track_id=tracking_id, report_id__link_id=tracking_id)
+    tracker = Tracking.objects.get(track_id=tracking_id, report_id__link_id=report_id)
     tracker.download_count += 1
     tracker.downloaded_at = datetime.datetime.now()
     tracker.save()
