f432c4f6537076bd29219d381ed43b9331ec29da
Suroor Hussain <suroorhussain@hamon.in>
2018-06-11 12:10:56 +0530

    Add the models from reports app to admin

---
+++ apps/reports/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Sending_Information, Tracking
+# Register your models here.
+
+admin.site.register(Sending_Information)
+admin.site.register(Tracking)
