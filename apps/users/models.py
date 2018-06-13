cfc0c788a57d631ffb1d5618c5c63da9c0eb559d
Ansal S <ansal@bssatech.com>
2018-06-13 14:41:47 +0530

    model, migrations for PreGroupUser

--- apps/users/models.py
+++ apps/users/models.py
@@ -8,7 +8,8 @@ from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager,
     AbstractBaseUser,
-    PermissionsMixin
+    PermissionsMixin,
+    Group
 )
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -128,7 +129,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __unicode__(self):
         return self.mobile_no
-    
+
     class Meta:
         ordering = ['id', ]
 
@@ -138,6 +139,7 @@ def user_created_verify_email(sender, instance=None, created=False, **kwargs):
     if created and instance.email:
         instance.send_verification_email()
 
+
 class UserBoundary(models.Model):
     user = models.ForeignKey('User')
     boundary = models.ForeignKey('boundary.Boundary')
@@ -145,3 +147,17 @@ class UserBoundary(models.Model):
     class Meta:
         unique_together = (('user', 'boundary'), )
 
+
+class PreGroupUser(models.Model):
+    """
+        A list of mobile numbers that will be added to a particular user group
+        when the user logins/signs up with a number frm the list.
+
+        This was initially added to add existing/new users who are part of the
+        new A3 assessment app to a new user group called a3_users.
+    """
+
+    mobile_no = models.CharField(max_length=32)
+    source = models.CharField(max_length=32)
+    group = models.ForeignKey(Group)
+    created = models.DateTimeField(auto_now_add=True)
