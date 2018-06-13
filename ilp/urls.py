a137fa72f7d060fc1c7c28e2b36159ae27fcb7b5
Hasna <hasna@hamon.in>
2018-06-13 15:51:35 +0530

    Add download url for analytics

--- ilp/urls.py
+++ ilp/urls.py
@@ -15,7 +15,7 @@ from users.views import (
     ProfileEditPageView
 )
 from assessments.views import SYSView, gka_dashboard
-from reports.views import view_report, download_report, SendReport, ReportAnalytics
+from reports.views import view_report, download_report, SendReport, ReportAnalytics, download_analytics
 
 
 api_docs_view = get_swagger_view(title='ILP API')
@@ -182,4 +182,5 @@ urlpatterns = [
     url(r'^reportgen/(?P<report_id>([a-z]|[0-9])+)/download', download_report, name='download_report_no_track_id'),
     url(r'^reportgen/(?P<report_id>([a-z]|[0-9])+)/(?P<tracking_id>([a-z]|[0-9])*)/download', download_report, name='download_report'),
     url(r'^reportanalytics/$', ReportAnalytics.as_view(), name='report_analytics'),
+    url(r'^reportanalytics/download$', download_analytics, name='download_analytics'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
