8ad0171e89392919ca9b72c27fffcc1aab0753c8
Suroor Hussain <suroorhussain@hamon.in>
2018-06-14 13:28:08 +0530

    Add the new reports to reportlist

--- apps/reports/reportlist.py
+++ apps/reports/reportlist.py
@@ -1,9 +1,14 @@
-from .reports import GPMathContestReport, SchoolReport, ClusterReport, BlockReport, DistrictReport
+from .reports import GPMathContestReport, SchoolReport, ClusterReport, BlockReport, DistrictReport, GPMathContestReportSummarized, SchoolReportSummarized, ClusterReportSummarized, BlockReportSummarized, DistrictReportSummarized
 reportlist = {"GPMathContestReport": GPMathContestReport,
               "SchoolReport":SchoolReport,
               "ClusterReport":ClusterReport,
               "BlockReport":BlockReport,
-              "DistrictReport":DistrictReport
+              "DistrictReport":DistrictReport,
+              "GPMathContestReportSummarized":GPMathContestReportSummarized,
+              "SchoolReportSummarized":SchoolReportSummarized,
+              "ClusterReportSummarized":ClusterReportSummarized,
+              "BlockReportSummarized":BlockReportSummarized,
+              "DistrictReportSummarized":DistrictReportSummarized,
 }
 param_ids = { "GPMathContestReport": "gp_name",
               "SchoolReport":"school_code",
