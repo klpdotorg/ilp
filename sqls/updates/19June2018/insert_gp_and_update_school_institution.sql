b8e947a436cca061f93d76eba4a22d59af79cfec
brijeshpoojary <brijesh@klp.org.in>
2018-06-19 18:55:17 +0530

    updating assessment_answergroup and schools_instituion

---
+++ sqls/updates/19June2018/insert_gp_and_update_school_institution.sql
@@ -0,0 +1,9 @@
+update schools_institution set gp_id=6206 where id in (24494,24504);
+
+update schools_institution set gp_id=2497 where id in (24239);
+
+update schools_institution set gp_id=1629 where id in (5248);
+
+insert into boundary_electionboundary (id,dise_slug,const_ward_name,const_ward_type_id,state_id,status_id) values (6211,'','K Ayyanahalli','GP','2','AC');
+
+update schools_institution set gp_id=6211 where id=3221;
