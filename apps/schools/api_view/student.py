f3a6000f74d58990ee745319701eb8263c402bbb
Nabeel Valapra <nabeelvalapra@gmail.com>
2018-06-25 10:34:09 +0530

    Returns object after creating student object

--- apps/schools/api_view/student.py
+++ apps/schools/api_view/student.py
@@ -64,10 +64,13 @@ class StudentViewSet(
         serializer = self.get_serializer(data=request.data, many=True)
         try:
             serializer.is_valid(raise_exception=True)
-            instance = self.perform_create(serializer)
+            self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
             return Response(
-                instance, status=status.HTTP_201_CREATED, headers=headers)
+                serializer.data,
+                status=status.HTTP_201_CREATED,
+                headers=headers
+            )
         except ValidationError as exception:
             logger.debug("Validation error during creation of student")
             raise ValidationError(exception.get_full_details())
