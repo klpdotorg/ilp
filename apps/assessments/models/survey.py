a85f96506ab94dd81650e2e8300fc05f14803c0c
Nabeel Valapra <nabeelvalapra@gmail.com>
2018-07-04 14:51:09 +0530

    empty commit to trigger circle ci

--- apps/assessments/models/survey.py
+++ apps/assessments/models/survey.py
@@ -1,5 +1,6 @@
-from django.db import models
 from users.models import User
+
+from django.db import models
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from django.utils import timezone
