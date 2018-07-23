a4190b1d6dc20920c08220743a0b62cfe57170f0
brijeshpoojary <brijesh@klp.org.in>
2018-07-24 01:24:25 +0530

    Inserting into survey_user_type_mapping

---
+++ sqls/updates/24July2018.sql
@@ -0,0 +1,7 @@
+--update assessments_surveyusertypemapping
+insert into assessments_surveyusertypemapping (survey_id,usertype_id) values (14,'CRP');
+insert into assessments_surveyusertypemapping (survey_id,usertype_id) values (14,'SSA');
+insert into assessments_surveyusertypemapping (survey_id,usertype_id) values (14,'BRP');
+insert into assessments_surveyusertypemapping (survey_id,usertype_id) values (14,'BEO');
+insert into assessments_surveyusertypemapping (survey_id,usertype_id) values (14,'DDPI');
+insert into assessments_surveyusertypemapping (survey_id,usertype_id) values (14,'NOD');
