f0b5e8d42f8111329c4145c48d8e1fa4eeb0e39f
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-08-06 15:07:37 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,5 +4,6 @@ WORK_DIR=/home/ubuntu/ilp
 mkdir -p $WORK_DIR
 chown -vc ubuntu:www-data $WORK_DIR || exit 1
 cd $WORK_DIR
-/usr/bin/virtualenv -p /usr/bin/python3 venv || exit 1
+#/usr/bin/virtualenv -p /usr/bin/python3 venv || exit1
+[[ -d venv ]] || /usr/bin/virtualenv -p /usr/bin/python3 venv
 chown -R ubuntu:www-data venv || exit 1
