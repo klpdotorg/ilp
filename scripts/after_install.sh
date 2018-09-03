5079d8db6c443b0df336ab449738583e12c76504
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-09-03 12:06:25 +0530

    Enable Debug mode to check permissions

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
+# enabling debug mode
+set -x
+whoami
+ls -la 
 WORK_DIR=/home/ubuntu/ilp
 cd $WORK_DIR
+whoami
+ls -la 
 source venv/bin/activate || exit 1
+whoami
+ls -la 
 pip3 install -r requirements/base.txt --exists-action w || exit 1
 pip3 install gunicorn || exit 1
 cp -fv /home/ubuntu/prod_settings.py ${WORK_DIR}/ilp/settings/
