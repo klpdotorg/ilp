5d334f4d2db8ac5fd0f4d29aaf3a52561b40ac3d
Ansal S <ansal@bssatech.com>
2018-09-06 11:39:22 +0530

    added user model to django admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,6 +2,12 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 
-from .models import PreGroupUser
+from .models import PreGroupUser, User
+
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('mobile_no', 'first_name', 'last_name',)
+
 
 admin.site.register(PreGroupUser)
+admin.site.register(User, UserAdmin)
