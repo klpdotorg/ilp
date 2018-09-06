59d1486587b08fe3569d2b720d201c0c33a23f8c
Ansal S <ansal@bssatech.com>
2018-09-06 11:31:32 +0530

    added PreGroupUser model to django admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
 
-# Register your models here.
+from .models import PreGroupUser
+
+admin.site.register(PreGroupUser)
