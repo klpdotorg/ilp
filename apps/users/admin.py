01999624f85eb2090750b25d25f358a0fc8cba68
Ansal S <ansal@bssatech.com>
2018-09-07 13:52:48 +0530

    re-enable admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -8,15 +8,23 @@ from easyaudit.models import CRUDEvent, LoginEvent, RequestEvent
 
 from .models import PreGroupUser, User, Group
 
+
 # Add PreGroupUser object so that program team members can
 # add teachers/users of A3 app
 class PreGroupUserAdmin(admin.ModelAdmin):
     search_fields = ('mobile_no',)
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('mobile_no',)
+
+
+# Register all used models
 admin.site.register(PreGroupUser, PreGroupUserAdmin)
+admin.site.register(User, UserAdmin)
 
 
 # Unregister all unwanted models
-admin.site.unregister(Group)
+# admin.site.unregister(Group)
 admin.site.unregister(Site)
 admin.site.unregister(Token)
 admin.site.unregister(CRUDEvent)
