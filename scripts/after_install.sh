3b9700fa32e71260471333288e8938a0a8e9e93f
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-09-10 11:23:35 +0530

    added configuration related to postman_test

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,3 +8,5 @@ cp -fv /home/ubuntu/prod_settings.py ${WORK_DIR}/ilp/settings/
 python3 manage.py migrate || exit 1
 python3 manage.py loaddata apps/*/fixtures/*.json || exit 1
 python3 manage.py collectstatic -c --noinput || exit 1
+npm install -g newman
+./runreadonlytests.sh environments/development_env.json
