9a75ba722eff597571749ce7673a11fe3957499a
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-09-12 15:35:39 +0530

    testing pastman configuration

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,3 +3,6 @@
 systemctl start ka.service || exit 1
 systemctl start od.service || exit 1
 systemctl start jk.service || exit 1
+npm install -g newman
+cd postman_tests
+./runreadonlytests.sh environments/development_env.json
