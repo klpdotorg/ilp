21e454f43fe68ae044d6e1bf33e5feaec8da99b5
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-09-14 11:02:21 +0530

    fixed permission issue

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,5 @@ systemctl start ka.service || exit 1
 systemctl start od.service || exit 1
 systemctl start jk.service || exit 1
 cd postman_tests
+chmod +x runreadonlytests.sh
 ./runreadonlytests.sh environments/development_env.json || exit 1
