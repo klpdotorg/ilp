4657fb2f71ec8af1009401d9285d19f1c898e2fc
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-09-14 14:34:14 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,5 +3,4 @@
 systemctl start ka.service || exit 1
 systemctl start od.service || exit 1
 systemctl start jk.service || exit 1
-cd $WORK_DIR/postman_tests
 ./runreadonlytests.sh environments/development_env.json || exit 1
