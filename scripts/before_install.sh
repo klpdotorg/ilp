57bb3db180aa9ae2fa160181620cccdb40f28778
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-09-14 11:28:35 +0530

    fixing permission issue

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,3 +6,6 @@ chown -vc ubuntu:www-data $WORK_DIR || exit 1
 cd $WORK_DIR
 [[ -d venv ]] || /usr/bin/virtualenv -p /usr/bin/python3 venv
 chown -R ubuntu:www-data venv || exit 1
+chown -R $USER ~/.npm || exit 1
+chown -R $USER /usr/lib/node_modules || exit 1
+chown -R $USER /usr/local/lib/node_modules || exit 1
