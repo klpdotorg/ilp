ab605f0c5ae84cfc4ba5cd6d5c0f9040eba6a9fe
mountainfirefly <pankajdcoder@gmail.com>
2018-09-14 09:41:46 +0530

    add coverage section template

--- assets/static/js/gp-contest/dashboard.js
+++ assets/static/js/gp-contest/dashboard.js
@@ -1,9 +1,17 @@
 'use strict';
-console.log('Coming here');
 
 (function() {
   klp.init = function() {
     let searchByGPs = false;
+    let selectedTab = '2016';
+    const years = ["2016", "2017", "2018"];
+    const tabs = years.map((tab) => {
+      return {
+        value: tab,
+        start_date: `${tab}-01-01`,
+        end_date: `${Number(tab) + 1}-01-01`
+      }
+    })
     const $educational_hierarchy_checkbox = $("#select-educational-hrc");
     const $gp_checkbox = $("#select-gp-checkbox");
     const $select_school_cont = $("#select-school-cont");
@@ -11,7 +19,6 @@ console.log('Coming here');
     const $select_gp_cont = $("#select-gp-cont");
 
     // Initialize accordion, filters and router
-    console.log(klp);
     klp.gp_filters.init();
     klp.router = new KLPRouter();
     klp.router.init();
@@ -39,6 +46,53 @@ console.log('Coming here');
       }
     }
 
+    // This function renders tabs
+    function renderYearsTabs() {
+      var tplYearTab = swig.compile($('#tpl-year-tabs').html());
+      var yearTabHTML = tplYearTab({ tabs: tabs });
+       
+      $('#year-tabs').html(yearTabHTML);
+    }
+
+    // This function select the tab
+    function selectYearTab(year) {
+      tabs.forEach((tab) => {
+        const $currentTab = $(`#${tab.value}-year`);
+        if (tab.value === year) {
+          $currentTab.addClass("selected-gp-tab");
+        } else {
+          $currentTab.removeClass("selected-gp-tab");
+        }
+      })
+    }
+
+    // Fetch coverage information
+    function loadCoverage() {
+      const selectedYearInfo = tabs.find((tab) => {
+        return tab.value === selectedTab;
+      });
+      console.log(selectedYearInfo, 'Printing the selectedYearInfo');
+
+      var $coverageXHR = klp.api.do(
+         `survey/summary/?survey_id=2&from=${selectedYearInfo.start_date}&${selectedYearInfo.end_date}&state=ka`
+      )
+
+      $coverageXHR.done(function(result) {
+        console.log(result);
+        var tplCoverage = swig.compile($('#tpl-coverage').html());
+        var coverageHTML = tplCoverage({ data: result.summary });
+        
+        $('#gp-coverage').html(coverageHTML);
+      });
+    }
+    // Calling all functions
+    hideSearchFields();
+    showDefaultFilters();
+    renderYearsTabs();
+    selectYearTab(selectedTab);
+    loadCoverage();
+
+    // listeners for checkboxes
     $educational_hierarchy_checkbox.on("change", function(value) {
       searchByGPs = false;
       $gp_checkbox.prop("checked", false);
@@ -53,8 +107,17 @@ console.log('Coming here');
       hideSearchFields();
     })
 
-    hideSearchFields();
-    showDefaultFilters();
+    // Button year listener
+    tabs.forEach((tab) => {
+      const $tabYearId = $(`#${tab.value}-year`);
+      $tabYearId.on("click", function(e) {
+        const selectedYear = e.target.dataset.value;
+        selectedTab = selectedYear;
+        selectYearTab(tab.value);
+        loadCoverage();
+      });
+    })
+
   }
 })()
 
