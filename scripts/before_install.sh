e99c938ea1bc0af2fc8dd65b4eeefc94edf6d488
anuthaharinimn <anuthanarayan21@gmail.com>
2018-09-21 11:57:30 +0530

    config changes in beforeinstall script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,7 @@ WORK_DIR=/home/ubuntu/ilp
 mkdir -p $WORK_DIR
 chown -vc ubuntu:www-data $WORK_DIR || exit 1
 cd $WORK_DIR
-[[ -d venv ]] || /usr/bin/virtualenv -p /usr/bin/python3 venv || exit 1
+[[ -d venv ]] || /usr/bin/virtualenv -p /usr/bin/python3 venv
 chown -R ubuntu:www-data venv || exit 1
 #chown -R ubuntu:www-data ~/.npm || exit 1
 #chown -R ubuntu:www-data /usr/lib/node_modules || exit 1
