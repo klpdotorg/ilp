a9e5a3825d132709698bbe85a0d6c392f0cc7956
anuthaharinimn <anuthanarayan21@gmail.com>
2018-09-24 15:13:20 +0530

    testing

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo test
+WORK_DIR=/home/ubuntu/ilp
+mkdir -p $WORK_DIR
+chown -vc ubuntu:www-data $WORK_DIR || exit 1
+cd $WORK_DIR
+[[ -d venv ]] || /usr/bin/virtualenv -p /usr/bin/python3 venv
+chown -R ubuntu:www-data venv || exit 1
