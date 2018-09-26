295ad0682cee584c27a08ce0c96cc71e5ca63a42
anuthaharinimn <anuthanarayan21@gmail.com>
2018-09-26 16:20:31 +0530

    reconfiguring postman test

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,6 +8,5 @@ cp -fv /home/ubuntu/prod_settings.py ${WORK_DIR}/ilp/settings/
 python3 manage.py migrate || exit 1
 python3 manage.py loaddata apps/*/fixtures/*.json || exit 1
 python3 manage.py collectstatic -c --noinput || exit 1
-#sudo npm install -g newman || exit 1
-#sudo npm install -g newman-reporter-html || exit 1
-#npm install -g newman || exit 1
+sudo npm install -g newman || exit 1
+sudo npm install -g newman-reporter-html || exit 1
