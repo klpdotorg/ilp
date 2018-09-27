8472241f2c3bbed9fb84eb20dc0aebd92ea08108
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-09-27 13:08:36 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,5 +3,11 @@
 systemctl start ka.service || exit 1
 systemctl start od.service || exit 1
 systemctl start jk.service || exit 1
-cd /home/ubuntu/ilp/postman_tests; ./runallreadonlytests.sh environments/development_env.json || exit 1
-echo $CIRCLE_BRANCH
+cd /home/ubuntu/ilp/postman_tests ##./runallreadonlytests.sh environments/development_env.json || exit 1
+if [[ "hostname" == "ilpdev" ]]; then
+  ./runallreadonlytests.sh environments/development_env.json
+ elif [ "hostname" == "ilp-staging" ]; then
+  ./runallreadonlytests.sh environments/staging_env.json
+ else
+    echo "None of the condition met"
+fi
