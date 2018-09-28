ad79e43e5c564cd542cca3866db9c5280fc0c65d
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-09-28 10:55:49 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,9 +4,9 @@ systemctl start ka.service || exit 1
 systemctl start od.service || exit 1
 systemctl start jk.service || exit 1
 cd /home/ubuntu/ilp/postman_tests ##./runallreadonlytests.sh environments/development_env.json || exit 1
-if [[ "hostname" == "ilpdev" ]]; then
+if [[ `hostname` == "ilpdev" ]]; then
   ./runallreadonlytests.sh environments/development_env.json
- elif [ "hostname" == "ilp-staging" ]; then
+ elif [[ `hostname` == "ilp-staging" ]]; then
   ./runallreadonlytests.sh environments/staging_env.json
  else
     echo "None of the condition met"
