ca9320bf0a4cca490790d560fe920e0b4d38e8e2
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-09-28 12:05:22 +0530

    mail configuratio

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,8 +6,16 @@ systemctl start jk.service || exit 1
 cd /home/ubuntu/ilp/postman_tests ##./runallreadonlytests.sh environments/development_env.json || exit 1
 if [[ `hostname` == "ilpdev" ]]; then
   ./runallreadonlytests.sh environments/development_env.json
+   m=$(echo $?)
  elif [[ `hostname` == "ilp-staging" ]]; then
   ./runallreadonlytests.sh environments/staging_env.json
+   m=$(echo $?)
  else
     echo "None of the condition met"
+fi || exit 1
+if [ $m -ne 0 ]
+then
+ zip -r test_results_summary test_results_summary.zip
+ mail -s "Warning: Postman tests failure post-deployment. Check deployment"  dev@klp.org.in -A test_results_summary.zip
 fi
+ 
