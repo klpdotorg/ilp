758eb8b12b0a059e9b59459e2d4b26c0a4f3115a
mountainfirefly <pankajdcoder@gmail.com>
2018-10-04 15:47:48 +0530

    fix: move about gp-contest section above the filter and make filters collapsible

--- assets/static/js/gp-contest/accordion.js
+++ assets/static/js/gp-contest/accordion.js
@@ -1,8 +1,8 @@
 /*http://www.jacklmoore.com/notes/jquery-accordion-tutorial/*/
 (function() {
-  var t = klp.accordion = {};
-  t.init = function() {
-      $('.js-accordion-container .js-accordion-header').click(function(e){
+    var t = klp.accordion = {};
+    t.init = function() {
+        $('.js-accordion-container .js-accordion-header').click(function(e){
           e.preventDefault();
           $(this)
               .closest('li')
@@ -16,6 +16,21 @@
           $(this).closest('li').find('.ct-chart').each(function(i, e) {
               if(e && e.__chartist__) { e.__chartist__.update(); }
           });
-      });
-  };
+        });
+        $('.js-accordion-container .js-accordion-header').click(function(e){
+            e.preventDefault();
+            $(this)
+                .closest('.search-container')
+                .find('.content')
+                .not(':animated')
+                .slideToggle('fast', function(){
+                    // klp.GP.accordionClicked($(this));
+                });
+
+            // //Update the chartist charts in this accordian section
+            // $(this).closest('li').find('.ct-chart').each(function(i, e) {
+            //     if(e && e.__chartist__) { e.__chartist__.update(); }
+            // });
+        });
+    };
 })();
