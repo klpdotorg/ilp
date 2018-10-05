2d087b6c8a4f6bcf40c5b007049fe35678420727
mountainfirefly <pankajdcoder@gmail.com>
2018-10-05 07:41:53 +0530

    fix: expend class 4 section by default

--- assets/static/js/gp-contest/accordion.js
+++ assets/static/js/gp-contest/accordion.js
@@ -24,13 +24,11 @@
                 .find('.content')
                 .not(':animated')
                 .slideToggle('fast', function(){
-                    // klp.GP.accordionClicked($(this));
                 });
-
-            // //Update the chartist charts in this accordian section
-            // $(this).closest('li').find('.ct-chart').each(function(i, e) {
-            //     if(e && e.__chartist__) { e.__chartist__.update(); }
-            // });
         });
+
+        // Expending class 4 selection by deafult
+        var class4LinkButton = $("#class-4-link-button");
+        class4LinkButton.closest('li').find('.content').not(':animated').slideToggle();
     };
 })();
