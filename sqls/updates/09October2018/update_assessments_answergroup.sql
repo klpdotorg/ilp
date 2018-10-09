042af23b95a90723b760e57ec276ed66b0190351
brijeshpoojary <brijesh@klp.org.in>
2018-10-09 12:31:05 +0530

    database updates for correcting gka dashboard counts

---
+++ sqls/updates/09October2018/update_assessments_answergroup.sql
@@ -0,0 +1,2 @@
+update assessments_answergroup_institution set institution_id=43018 where questiongroup_id in (45,46,47) and date_of_visit > '01/06/2018' and institution_id=42018;
+update assessments_answergroup_institution set institution_id=4991 where questiongroup_id in (45,46,47) and date_of_visit > '01/06/2018' and institution_id=49941;
