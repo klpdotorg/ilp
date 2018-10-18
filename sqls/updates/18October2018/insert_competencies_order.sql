a02466aa3524dc2a29f984c5118f74bae05536e9
shivangi <shivangi@klp.org.in>
2018-10-18 08:45:26 +0530

    Mappings added

---
+++ sqls/updates/18October2018/insert_competencies_order.sql
@@ -0,0 +1,40 @@
+insert into assessments_competencyorder(questiongroup_id,key,order) values(21,'Number Recognition', 1);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(21,'Place Value', 2);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(21,'Addition', 3);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(21,'Subtraction', 4);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(21,'Multiplication', 5);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(21,'Division', 6);
+
+
+insert into assessments_competencyorder(questiongroup_id,key,order) values(22,'Number Recognition', 1);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(22,'Addition', 2);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(22,'Subtraction', 3);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(22,'Multiplication', 4);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(22,'Division', 5);
+
+insert into assessments_competencyorder(questiongroup_id,key,order) values(23,'Number Recognition', 1);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(23,'Addition', 2);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(23,'Subtraction', 3);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(23,'Multiplication', 4);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(23,'Division', 5);
+
+insert into assessments_competencyorder(questiongroup_id,key,order) values(45,'Number Recognition', 1);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(45,'Place Value', 2);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(45,'Addition', 3);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(45,'Subtraction', 4);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(45,'Multiplication', 5);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(45,'Division', 6);
+
+insert into assessments_competencyorder(questiongroup_id,key,order) values(46,'Number Recognition', 1);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(46,'Place Value', 2);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(46,'Addition', 3);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(46,'Subtraction', 4);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(46,'Multiplication', 5);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(46,'Division', 6);
+
+insert into assessments_competencyorder(questiongroup_id,key,order) values(47,'Number Recognition', 1);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(47,'Place Value', 2);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(47,'Addition', 3);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(47,'Subtraction', 4);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(47,'Multiplication', 5);
+insert into assessments_competencyorder(questiongroup_id,key,order) values(47,'Division', 6);
---
+++ sqls/updates/18October2018/insert_competencies_questionmap.sql
@@ -0,0 +1,58 @@
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(21,'Number Recognition',271)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(21,'Number Recognition',272)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(21,'Place Value',273)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(21,'Place Value',274)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(21,'Addition',276)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(21,'Addition',277)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(21,'Subtraction',278)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(21,'Subtraction',279)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(21,'Multiplication',280)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(21,'Division',282)
+
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(22,'Number Recognition',271)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(22,'Number Recognition',272)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(22,'Addition',276)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(22,'Addition',277)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(22,'Subtraction',278)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(22,'Subtraction',279)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(22,'Multiplication',280)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(22,'Division',282)
+
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(23,'Number Recognition',271)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(23,'Number Recognition',272)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(23,'Addition',276)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(23,'Addition',277)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(23,'Subtraction',278)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(23,'Subtraction',279)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(23,'Multiplication',280)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(23,'Division',282)
+
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(45,'Number Recognition',271)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(45,'Place Value',273)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(45,'Addition',276)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(45,'Subtraction',278)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(45,'Multiplication',280)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(45,'Addition',277)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(45,'Subtraction',279)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(45,'Division',283)
+
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(46,'Number Recognition',271)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(46,'Addition',276)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(46,'Subtraction',278)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(46,'Multiplication',280)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(46,'Division',282)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(46,'Place Value',273)
+
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(47,'Number Recognition',271)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(47,'Addition',276)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(47,'Subtraction',278)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(47,'Multiplication',280)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(47,'Division',282)
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id) values(47,'Place Value',272)
+
+
+
+
+
+
+
