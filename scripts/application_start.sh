bf6091da59d974d347e0730076645e4adb0f1fdf
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-10-25 15:45:09 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,6 +4,7 @@ systemctl start ka.service || exit 1
 systemctl start od.service || exit 1
 systemctl start jk.service || exit 1
 cd /home/ubuntu/ilp/postman_tests ##./runallreadonlytests.sh environments/development_env.json || exit 1
+rm -rf test_results_summary.zip
 if [[ `hostname` == "ilpdev" ]]; then
   ./runallreadonlytests.sh environments/development_env.json
    m=$(echo $?)
@@ -16,7 +17,6 @@ fi || exit 1
 if [ $m -ne 0 ]
 then
  #zip -r test_results_summary test_results_summary.zip
- rm -rf test_results_summary.zip
  zip -r test_results_summary . -i test_results_summary.zip
  mail -s "Warning: Postman tests failure post-deployment. Check deployment"  dev@klp.org.in -A test_results_summary.zip
 fi
