4d1770fbe22793511294fcd049b895cd96fc57e0
Ansal <ansal@bssatech.com>
2018-10-27 17:55:24 +0530

    disabled postman tests

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,5 +6,5 @@ chown -vc ubuntu:www-data $WORK_DIR || exit 1
 cd $WORK_DIR
 [[ -d venv ]] || /usr/bin/virtualenv -p /usr/bin/python3 venv
 chown -R ubuntu:www-data venv || exit 1
-chown -R ubuntu:www-data ~/.npm || exit 1
-chown -R ubuntu:www-data /usr/lib/node_modules || exit 1
+# chown -R ubuntu:www-data ~/.npm || exit 1
+# chown -R ubuntu:www-data /usr/lib/node_modules || exit 1
