41edf24e501df0eea0bea39da731296cc3346d17
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-10-28 17:20:08 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,6 +6,7 @@ systemctl start jk.service || exit 1
 
 cd /home/ubuntu/ilp/postman_tests ##./runallreadonlytests.sh environments/development_env.json || exit 1
 rm -rf test_results_summary.zip
+rm -rf test_results_summary
 if [[ `hostname` == "ilpdev" ]]; then
   ./runallreadonlytests.sh environments/development_env.json
     m=$(echo $?)
