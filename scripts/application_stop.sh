00f0595042d811494a69242fe99a00640e84b0b4
anuthanarayanmn <36470137+anuthaharinimn@users.noreply.github.com>
2018-11-12 15:35:33 +0530

    fixing dev-dep issue

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-systemctl stop ka.service || exit 1
-systemctl stop od.service || exit 1
-systemctl stop jk.service || exit 1
+sudo systemctl stop ka.service || exit 1
+sudo systemctl stop od.service || exit 1
+sudo systemctl stop jk.service || exit 1
 
