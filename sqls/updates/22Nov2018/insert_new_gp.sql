162a35b5d23a18bf37c23fefb8bf3d0068c7bcdb
brijeshpoojary <brijesh@klp.org.in>
2018-11-22 22:59:48 +0530

    removed update files

--- sqls/updates/22Nov2018/insert_new_gp.sql
+++
@@ -1,5 +0,0 @@
---insert new gp
-insert into boundary_electionboundary (id,dise_slug,const_ward_name,const_ward_type_id,state_id,status_id) values (6258,'','Agadi (h)','GP','2','AC');
-insert into boundary_electionboundary (id,dise_slug,const_ward_name,const_ward_type_id,state_id,status_id) values (6259,'','B.aralikatti','GP','2','AC');
-insert into boundary_electionboundary (id,dise_slug,const_ward_name,const_ward_type_id,state_id,status_id) values (6260,'','Aralikatti (h)','GP','2','AC');
-insert into boundary_electionboundary (id,dise_slug,const_ward_name,const_ward_type_id,state_id,status_id) values (6261,'','Harave (mys)','GP','2','AC');
--- sqls/updates/22Nov2018/update_schools_gp_mapping.sql
+++
@@ -1,5 +0,0 @@
---update schools_institution
-update schools_institution set gp_id=6258 where id in (50764,50795,50763,50762,50810,50761,50769,50766);
-update schools_institution set gp_id=6259 where id in (35171,35160,35161,35256);
-update schools_institution set gp_id=6260 where id in (50951,50943,50944);
-update schools_institution set gp_id=6261 where id in (54405,54376,54375,54377);
