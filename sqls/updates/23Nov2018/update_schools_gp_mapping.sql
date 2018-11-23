5f04ec8cb907ef0469a58f5c29b96495489cdf09
brijeshpoojary <brijesh@klp.org.in>
2018-11-23 14:01:28 +0530

    Insert new gp and update schools mapping

---
+++ sqls/updates/23Nov2018/update_schools_gp_mapping.sql
@@ -0,0 +1,8 @@
+--update schools_institution
+update schools_institution set gp_id=6258 where id in (50764,50795,50763,50762,50810,50761,50769,50766);
+update schools_institution set gp_id=6259 where id in (35171,35160,35161,35256);
+update schools_institution set gp_id=6260 where id in (50951,50943,50944);
+update schools_institution set gp_id=6261 where id in (54405,54376,54375,54377);
+update schools_institution set gp_id=6262 where id in (37774,37769);
+update schools_institution set gp_id=6263 where id in (25679,25678,25825,25694);
+update schools_institution set gp_id=2757 where id=43144;
