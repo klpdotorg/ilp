231c4ba685aac47c91b6581b3231e6292b12dd54
brijeshpoojary <brijesh@klp.org.in>
2018-11-29 19:30:28 +0530

    updating gp contest data in db

---
+++ sqls/updates/29Nov2018/insert_new_gp.sql
@@ -0,0 +1,2 @@
+--insert new gp
+insert into boundary_electionboundary (id,dise_slug,const_ward_name,const_ward_type_id,state_id,status_id) values (6264,'','Mangala (ch)','GP','2','AC');
---
+++ sqls/updates/29Nov2018/update_date_of_visit.sql
@@ -0,0 +1,24 @@
+--update assessment_answergroup_institution
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-01' where questiongroup_id in (47) and institution_id=35237 and date_of_visit::timestamp BETWEEN to_timestamp('01/09/2018','dd/mm/yyyy') and to_timestamp('12/09/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-08-07' where questiongroup_id in (46) and institution_id in (5096,5098,5095) and date_of_visit::timestamp BETWEEN to_timestamp('01/07/2018','dd/mm/yyyy') and to_timestamp('08/07/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-27' where questiongroup_id in (45) and institution_id in (11110
+,11102,11108) and date_of_visit::timestamp BETWEEN to_timestamp('25/08/2018','dd/mm/yyyy') and to_timestamp('28/08/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-11' where questiongroup_id in (46) and institution_id in (12476
+,12471,44997,12473,12468,12458,12475) and date_of_visit::timestamp BETWEEN to_timestamp('01/08/2018','dd/mm/yyyy') and to_timestamp('12/08/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-11' where questiongroup_id in (47) and institution_id in (12471,12475,12476) and date_of_visit::timestamp BETWEEN to_timestamp('01/08/2018','dd/mm/yyyy') and to_timestamp('12/08/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-24' where questiongroup_id in (46) and institution_id=36172 and date_of_visit::timestamp BETWEEN to_timestamp('20/09/2018','dd/mm/yyyy') and to_timestamp('30/09/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-24' where questiongroup_id in (46) and institution_id=36176 and date_of_visit::timestamp BETWEEN to_timestamp('20/08/2018','dd/mm/yyyy') and to_timestamp('30/08/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-08-02' where questiongroup_id in (45) and institution_id in (4186,4195) and date_of_visit::timestamp BETWEEN to_timestamp('01/07/2018','dd/mm/yyyy') and to_timestamp('03/07/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-08-29' where questiongroup_id in (45) and institution_id in (24411) and date_of_visit::timestamp BETWEEN to_timestamp('25/08/2018','dd/mm/yyyy') and to_timestamp('29/08/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-25' where questiongroup_id in (47) and institution_id in (24592
+,24580,24581,24596) and date_of_visit::timestamp BETWEEN to_timestamp('24/08/2018','dd/mm/yyyy') and to_timestamp('26/08/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-14' where questiongroup_id in (47) and institution_id=12993 and date_of_visit::timestamp BETWEEN to_timestamp('13/08/2018','dd/mm/yyyy') and to_timestamp('15/08/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-11' where questiongroup_id in (45) and institution_id in (13373,13380,13376,13385,45164,13384,45163,13383,45162,13382,45165,13381) and date_of_visit::timestamp BETWEEN to_timestamp('08/11/2018','dd/mm/yyyy') and to_timestamp('10/11/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-27' where id in (1296447,1296449);
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-25' where questiongroup_id in (45) and institution_id=10788
+ and date_of_visit::timestamp BETWEEN to_timestamp('14/09/2018','dd/mm/yyyy') and to_timestamp('21/09/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-25' where questiongroup_id in (47) and institution_id=10788
+ and date_of_visit::timestamp BETWEEN to_timestamp('31/08/2018','dd/mm/yyyy') and to_timestamp('02/09/2018','dd/mm/yyyy');
+update assessments_answergroup_institution set date_of_visit=Timestamp '2018-09-29' where questiongroup_id in (46,47) and institution_id=6976
+ and date_of_visit::timestamp BETWEEN to_timestamp('30/09/2018','dd/mm/yyyy') and to_timestamp('10/10/2018','dd/mm/yyyy');
+
