81ed7f71e79bbe4a2e60c4373573d143fed4acd8
shivangi <shivangi@klp.org.in>
2018-12-06 10:04:49 +0530

    Added 2 more updates

--- apps/assessments/management/commands/update_questiondetails.py
+++ apps/assessments/management/commands/update_questiondetails.py
@@ -4,7 +4,7 @@ from io import StringIO
 
 from django.utils import timezone
 from django.core.management.base import BaseCommand, CommandError
-from assessments.models import QuestionGroup, Question, QuestionGroup_Questions, Concept, MicroConceptGroup, MicroConcept, QuestionLevel, QuestionInformationType, LearningIndicator
+from assessments.models import QuestionGroup, Question, QuestionGroup_Questions, Concept, MicroConceptGroup, MicroConcept, QuestionLevel, QuestionInformationType, LearningIndicator, AnswerInstitution, CompetencyQuestionMap
 from common.models import Status
 
 
@@ -30,7 +30,7 @@ class Command(BaseCommand):
     def get_question(self, concept, microconceptgroup, microconcept, questionlevel, questioninformationtype, learningindicator, questiongroup, sequence):
         question = QuestionGroup_Questions.objects.get(questiongroup = questiongroup, sequence=sequence).question
         if question.concept == '' or question.concept == None:
-            print("updating")
+            print("updating for questiongroup: "+str(questiongroup.id)+", question: "+str(question.id)+", sequence: "+str(sequence))
             question.concept_id = concept.pk
             question.microconcept_group_id = microconceptgroup.pk
             question.microconcept_id = microconcept.pk
@@ -41,10 +41,10 @@ class Command(BaseCommand):
             return question, False 
         else:
             if question.concept == concept and question.microconcept_group == microconceptgroup and question.microconcept == microconcept and question.question_level == questionlevel and question.question_info_type == questioninformationtype and question.learning_indicator == learningindicator:
-                print("present")
+                print("present for questiongroup: "+str(questiongroup.id)+", question: "+str(question.id)+", sequence: "+str(sequence))
                 return question, False 
             else:
-                print("creating question")
+                print("creating question for questiongroup: "+str(questiongroup.id)+", question: "+str(question.id)+", sequence: "+str(sequence))
                 question = Question.objects.create(question_text=microconcept.description, display_text=microconcept.description, is_featured='True',status = Status.objects.get(pk='AC'), concept_id=concept.pk, microconcept_group_id=microconceptgroup.pk, microconcept_id=microconcept.pk, question_level_id=questionlevel.pk, question_info_type_id=questioninformationtype.pk,learning_indicator_id=learningindicator.pk)
                 return question, True 
             
@@ -68,6 +68,18 @@ class Command(BaseCommand):
             question, created = self.get_question(concept, microconceptgroup, microconcept, questionlevel, questioninformationtype, learningindicator, questiongroup, sequence)
          
             if created:
+                print("New question id: "+str(question.id))
+                ansinst = AnswerInstitution.objects.filter(answergroup__questiongroup=questiongroup,question__questiongroup_questions__sequence=sequence, question__questiongroup_questions__questiongroup=questiongroup)
+                print("Update answers: "+str(ansinst.count()))
+                for ans in ansinst:
+                    ans.question = question
+                    ans.save()
+                competencyqmap = CompetencyQuestionMap.objects.filter(questiongroup = questiongroup, question__questiongroup_questions__sequence=sequence, question__questiongroup_questions__questiongroup=questiongroup)
+                print("Update CompetencyQuestionMap: "+str(competencyqmap.count()))
+                for qentry in competencyqmap:
+                    qentry.question = question
+                    qentry.save()
+                
                 print("updating questiongroup: "+str(questiongroup.id)+" "+str(sequence))
                 qgq = QuestionGroup_Questions.objects.get(questiongroup=questiongroup, sequence=sequence)
                 qgq.question = question
