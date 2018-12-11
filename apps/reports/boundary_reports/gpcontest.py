09f6c03d18758de87e6c64bf732a85ac702f482d
Subhashini Srinivasan <subha@klp.org.in>
2018-12-11 13:28:21 +0530

    fix for gender agg bug in Od reports

--- apps/reports/boundary_reports/gpcontest.py
+++ apps/reports/boundary_reports/gpcontest.py
@@ -141,7 +141,7 @@ class GPMathContestReport(BaseReport):
                                                             .filter(yearmonth__lte = self.report_to)
         gender_agg = SurveyInstitutionQuestionGroupGenderAgg.objects.filter(
                 institution_id__gp=gp_obj, 
-                survey_id=2, 
+                survey_id=self.gpcontest_survey_id, 
                 yearmonth__gte=self.report_from,
                 yearmonth__lte=self.report_to)        
         
