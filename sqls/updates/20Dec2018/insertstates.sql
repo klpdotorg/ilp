dc97510d8fafac6f97b89436540c456da29ed8ad
shivangi <shivangi@klp.org.in>
2018-12-20 22:02:00 +0530

    Added missed statecode updates for AP and GJ

--- sqls/updates/20Dec2018/insertstates.sql
+++ sqls/updates/20Dec2018/insertstates.sql
@@ -1,2 +1,4 @@
 insert into boundary_boundary(id,name, dise_slug, boundary_type_id,parent_id,status_id) values(5, 'andhrapradesh', 'AndhraPradesh', 'ST', 1, 'AC'); 
 insert into boundary_boundary(id,name, dise_slug, boundary_type_id,parent_id,status_id) values(6, 'gujarat', 'Gujarat', 'ST', 1, 'AC'); 
+insert into boundary_boundarystatecode values('ap',5,'tel');
+insert into boundary_boundarystatecode values('gj',6,'guj');
