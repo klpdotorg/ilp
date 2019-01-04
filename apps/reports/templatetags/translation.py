ae8d72757bd83945bb8b600690691b9f58a24cbb
Subhashini Srinivasan <subha@klp.org.in>
2019-01-04 09:47:59 +0530

    Fixed Place Value issue with reports

--- apps/reports/templatetags/translation.py
+++ apps/reports/templatetags/translation.py
@@ -9,3 +9,14 @@ def translate(text):
 @register.filter(name='to_float')
 def to_float(value):
     return float(value)
+
+'''This filter is very specific to our JSOn structure of reports. 
+If something changes in the JSON, this method will have to change
+FIrst argument is a JSOn structure. Second argument is the question_key'''
+@register.simple_tag
+def questionkey_exists(data, question_key):
+    exists = "False"
+    for val in data:
+        if val["contest"] == question_key:
+            exists = "True"
+    return exists
