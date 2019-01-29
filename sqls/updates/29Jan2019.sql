25d48421e445e09722a8480a2de6d787a270d497
brijeshpoojary <brijesh@klp.org.in>
2019-01-29 15:17:06 +0530

    Update 29Jan2019.sql

--- sqls/updates/29Jan2019.sql
+++ sqls/updates/29Jan2019.sql
@@ -5,10 +5,10 @@ update assessments_survey set name='ALP Class Visit' where id=22;
 update assessments_survey set lang_name='ALP વર્ગ મુલાકાત' where id=22;
 
 --update assessment_questiongroup
-update assessments_questiongroup set name='Ganitha Mithra Class Visit' where id=54;
-update assessments_questiongroup set lang_name='గణిత మిత్ర తరగతి సందర్శన' where id=54;
-update assessments_questiongroup set name='ALP Class Visit' where id=55;
-update assessments_questiongroup set lang_name='ALP વર્ગ મુલાકાત' where id=55;
+update assessments_questiongroup set name='Ganitha Mithra Class Visit' where id=53;
+update assessments_questiongroup set lang_name='గణిత మిత్ర తరగతి సందర్శన' where id=53;
+update assessments_questiongroup set name='ALP Class Visit' where id=54;
+update assessments_questiongroup set lang_name='ALP વર્ગ મુલાકાત' where id=54;
 
 --update assessment_question
 update assessments_question set question_text='Do you think your child can read Gujarati?' where id=800;
