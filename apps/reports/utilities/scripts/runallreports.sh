b5c9e3d4438b6f9210e254adf208cddc31a0cce8
Subhashini Srinivasan <subha@klp.org.in>
2019-02-07 10:38:19 +0530

    Utility scripts for generating reports

--- apps/reports/utilities/scripts/runallreports.sh
+++ apps/reports/utilities/scripts/runallreports.sh
@@ -7,63 +7,67 @@ if [ $# -eq 0 ]
     echo "- param1: Starting date of reports in yyyymm format. Eg. 201706"
     echo "- param2: End date of reports in yyyymm format. Eg. 201809"
     echo "- param3: Directory where all the contact CSV files reside. No Slash at the end. Eg. /home/ubuntu"
+    echo "- param4: two character state code. Eg. ka, od etc.."
     exit 1
 fi
 FROM=$1
 TO=$2
 CSV_DIR_PATH=$3
+STATE_CODE = $4
+# All config files are named in the following format: {statecode}_reports.ini
+CONFIG_FILE = "apps/reports/config/" + $STATE_CODE + "_reports.ini"
 
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 echo "Starting district  reports run $TIMESTAMP"
-python manage.py sendreports DistrictReport --from=$FROM --to=$TO --config="apps/reports/config/ka_reports.ini" "$CSV_DIR_PATH/1million_districts.csv" 
+python manage.py sendreports DistrictReport --from=$FROM --to=$TO --config=$CONFIG_FILE "$CSV_DIR_PATH/1million_districts.csv" 
 echo "Done sending reports"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 echo $TIMESTAMP      
 echo "================================"
 echo "Starting district summarized  reports run `date '+%Y-%m-%d %H:%M:%S'`"
-python manage.py sendreports DistrictReportSummarized --from=$FROM --to=$TO --config="apps/reports/config/ka_reports.ini" "$CSV_DIR_PATH/1million_districts.csv"
+python manage.py sendreports DistrictReportSummarized --from=$FROM --to=$TO --config=$CONFIG_FILE "$CSV_DIR_PATH/1million_districts.csv"
 echo "Done sending reports"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 echo $TIMESTAMP  
 echo "================================"
 echo "Starting block  reports run `date '+%Y-%m-%d %H:%M:%S'`"
-python manage.py sendreports BlockReport --from=$FROM --to=$TO --config="apps/reports/config/ka_reports.ini" "$CSV_DIR_PATH/1million_blocks.csv"
+python manage.py sendreports BlockReport --from=$FROM --to=$TO --config=$CONFIG_FILE "$CSV_DIR_PATH/1million_blocks.csv"
 echo "Done sending reports"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 echo $TIMESTAMP
 echo "================================"
 echo "Starting block summarized reports run `date '+%Y-%m-%d %H:%M:%S'`"
-python manage.py sendreports BlockReportSummarized --from=$FROM --to=$TO --config="apps/reports/config/ka_reports.ini" "$CSV_DIR_PATH/1million_blocks.csv"
+python manage.py sendreports BlockReportSummarized --from=$FROM --to=$TO --config=$CONFIG_FILE "$CSV_DIR_PATH/1million_blocks.csv"
 echo "Done sending reports"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 echo $TIMESTAMP
 echo "================================"
 echo "Starting cluster  reports run `date '+%Y-%m-%d %H:%M:%S'`"
-python manage.py sendreports ClusterReport --from=$FROM --to=$TO --config="apps/reports/config/ka_reports.ini" "$CSV_DIR_PATH/1million_clusters.csv"
+python manage.py sendreports ClusterReport --from=$FROM --to=$TO --config=$CONFIG_FILE "$CSV_DIR_PATH/1million_clusters.csv"
 echo "Done sending reports"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 echo $TIMESTAMP      
 echo "================================"
 echo "Starting Cluster SUMMARIZED reports run `date '+%Y-%m-%d %H:%M:%S'`"
-python manage.py sendreports ClusterReportSummarized --from=$FROM --to=$TO --config="apps/reports/config/ka_reports.ini" "$CSV_DIR_PATH/1million_clusters.csv"
+python manage.py sendreports ClusterReportSummarized --from=$FROM --to=$TO --config=$CONFIG_FILE "$CSV_DIR_PATH/1million_clusters.csv"
 echo "Done sending reports"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 echo $TIMESTAMP
 echo "================================"
 echo "Starting GPContest reports run `date '+%Y-%m-%d %H:%M:%S'`"
-python manage.py sendreports GPMathContestReport --from=$FROM --to=$TO --config="apps/reports/config/ka_reports.ini" "$CSV_DIR_PATH/1million_gps.csv"
+python manage.py sendreports GPMathContestReport --from=$FROM --to=$TO --config=$CONFIG_FILE "$CSV_DIR_PATH/1million_gps.csv"
 echo "Done sending reports"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 echo $TIMESTAMP
 echo "================================"
 echo "Starting GPContest summarized reports run `date '+%Y-%m-%d %H:%M:%S'`"
-python manage.py sendreports GPMathContestReportSummarized --from=$FROM --to=$TO --config="apps/reports/config/ka_reports.ini" "$CSV_DIR_PATH/1million_gps.csv"
+python manage.py sendreports GPMathContestReportSummarized --from=$FROM --to=$TO --config=$CONFIG_FILE "$CSV_DIR_PATH/1million_gps.csv"
 echo "Done sending reports"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 echo $TIMESTAMP
 echo "================================"
 echo "Starting School reports run `date '+%Y-%m-%d %H:%M:%S'`"
-python manage.py sendreports SchoolReport --from=$FROM --to=$TO --config="apps/reports/config/ka_reports.ini" "$CSV_DIR_PATH/1million_schools.csv"
+python manage.py sendreports SchoolReport --from=$FROM --to=$TO --config=$CONFIG_FILE "$CSV_DIR_PATH/1million_schools.csv"
 echo "Done sending reports"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 echo $TIMESTAMP
