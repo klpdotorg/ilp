206b1f6d6dfa788c0147da608cb542750b234fb1
Subhashini Srinivasan <subha@klp.org.in>
2019-04-03 10:20:05 +0530

    First cut of gp contest hardcopy reports

---
+++ apps/reports/scripts/test_basic_gp_info.py
@@ -0,0 +1,12 @@
+from reports.gp_hardcopy_reports.gp_compute_numbers import *
+
+
+def run():
+    print("BASIC GP INFO")
+    try:
+        gp_info = get_general_gp_info(1035, 1819)
+    except ValueError as e:
+        print(e)
+    else:
+        print(gp_info)
+        print("=================================================")
