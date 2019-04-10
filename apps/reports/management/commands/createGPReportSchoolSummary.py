ba8c54e6c106c63e78c1d00c33df10913e113419
shivangi <shivangi@klp.org.in>
2019-04-10 11:52:26 +0530

    Added template for Report School Summary Sheet

--- apps/reports/management/commands/createGPReportSchoolSummary.py
+++ apps/reports/management/commands/createGPReportSchoolSummary.py
@@ -6,20 +6,17 @@ import shutil
 import sys
 from schools.models import Institution
 from django.core.management.base import BaseCommand
+from datetime import datetime
 
 
 class Command(BaseCommand):
-    basefiledir= os.path.dirname(os.path.realpath(__file__))+"/forms/gpcontest"
+    basefiledir= os.path.dirname(os.path.realpath(__file__))+"/gpcontest"
     templatedir = "/templates/"
     outputdir = "/pdfs/"
-    out_file = "gpsummarysheet"
-    template_name = "GPContestSummarySheet.tex"
+    out_file = "gpreport_schoolsummary"
+    template_name = "GPReportSchoolSummary.tex"
     template_file = basefiledir+templatedir+template_name
     build_d = basefiledir+"/build"
-
-    def add_arguments(self, parser):
-        parser.add_argument('gpid')
-
 
     def initiatelatex(self):
         if not os.path.exists(self.build_d):  # create the build directory if not existing
@@ -39,33 +36,25 @@ class Command(BaseCommand):
         template = latex_jinja_env.get_template(self.template_file)
         return template
 
-    def getBoundaryInfo(self, gpid):
-        boundaryqs = Institution.objects.filter(gp_id=gpid).values('admin1_id__name','admin2_id__name','gp_id__const_ward_name').distinct()
-        if boundaryqs.count() > 1:
-            print("More than one set of values returned for gpid: "+str(gpid)+" : "+str(boundaryqs))
-            exit
-        print(boundaryqs)
-        for boundary in boundaryqs:
-            boundaryinfo= {"district": boundary["admin1_id__name"].title(), "block": boundary["admin2_id__name"].title(), "gpid":str(gpid), "gpname":boundary["gp_id__const_ward_name"].title()}
-        return boundaryinfo
-
-    def getSchoolInfo(self, gpid):
-        schoolinfo = []
-        schoolsqs = Institution.objects.filter(gp_id=gpid).values('name','dise_id__school_code').distinct()
-        for school in schoolsqs:
-            schoolinfo.append({"schoolname": school['name'], "disecode": school['dise_id__school_code']})
-        return schoolinfo
-
     def handle(self, *args, **options):
         gpid = options.get("gpid")
         self.out_file = self.out_file+"_"+str(gpid)
         template = self.initiatelatex()
-        boundaryinfo = self.getBoundaryInfo(gpid)
-        schoolinfo = self.getSchoolInfo(gpid)
-        renderer_template = template.render(boundaryinfo=boundaryinfo, schools=schoolinfo)
+        info = {"date": datetime.today().strftime('%d-%m-%Y')}
+        schoolinfo = [{"gpid": "GPID", "gpname": "GPNAME",
+                      "schoolname": "SNAME", "dise_code": "DISE",
+                      "class4": 10, "class5": 15, "class6": 20,
+                      "generated":45, "sent_to": "Field Name"},
+                     {"gpid": "GPID", "gpname": "GPNAME",
+                      "schoolname": "SNAME1", "dise_code": "DISE1",
+                      "class4": 10, "class5": 15, "class6": 20,
+                      "generated":45, "sent_to": "Field Name"}]
+        renderer_template = template.render(schools=schoolinfo, info=info)
 
         with open(self.out_file+".tex", "w", encoding='utf-8') as f:  # saves tex_code to outpout file
             f.write(renderer_template)
 
         os.system("xelatex -output-directory {} {}".format(os.path.realpath(self.build_d), os.path.realpath(self.out_file)))
+        print(os.path.dirname(self.basefiledir+self.outputdir))
         shutil.copy2(self.build_d+"/"+self.out_file+".pdf", os.path.dirname(self.basefiledir+self.outputdir))
+        print("finished copying")
