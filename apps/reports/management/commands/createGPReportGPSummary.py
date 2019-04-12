de873b74faea949ca798f8aba58d5c7c65ed0a01
shivangi <shivangi@klp.org.in>
2019-04-12 14:07:12 +0530

    GPReportGPSummary templates and code

--- apps/reports/management/commands/createGPReportGPSummary.py
+++ apps/reports/management/commands/createGPReportGPSummary.py
@@ -9,8 +9,8 @@ class Command(BaseCommand):
     basefiledir = os.path.dirname(os.path.realpath(__file__))+"/gpcontest"
     templatedir = "/templates/"
     outputdir = "/pdfs/"
-    out_file = "gpreport_schoolsummary"
-    template_name = "GPReportSchoolSummary.tex"
+    out_file = "gpreport_gpsummary_"
+    template_name = "GPReportGPSummary.tex"
     template_file = basefiledir+templatedir+template_name
     build_d = basefiledir+"/build"
 
@@ -39,15 +39,15 @@ class Command(BaseCommand):
         self.out_file = self.out_file+"_"+str(gpid)
         template = self.initiatelatex()
         info = {"date": datetime.today().strftime('%d-%m-%Y')}
-        schoolinfo = [{"gpid": "GPID", "gpname": "GPNAME",
-                       "schoolname": "SNAME", "dise_code": "DISE",
-                       "class4": 10, "class5": 15, "class6": 20,
-                       "generated": 45, "sent_to": "Field Name"},
-                      {"gpid": "GPID", "gpname": "GPNAME",
-                       "schoolname": "SNAME1", "dise_code": "DISE1",
-                       "class4": 10, "class5": 15, "class6": 20,
-                       "generated": 45, "sent_to": "Field Name"}]
-        renderer_template = template.render(schools=schoolinfo, info=info)
+        gpinfo = [{"gpid": "GPID", "gpname": "GPNAME",
+                   "total_schools": 100, "class4_schools": 25,
+                   "class5_schools": 25, "class6_schools": 50,
+                   "generated": 1, "sent_to": "Field Name"},
+                  {"gpid": "GPID1", "gpname": "GPNAME1",
+                   "total_schools": 80, "class4_schools": 20,
+                   "class5_schools": 40, "class6_schools": 20,
+                   "generated": 1, "sent_to": "Field Name1"}]
+        renderer_template = template.render(gps=gpinfo, info=info)
 
         # saves tex_code to outpout file
         with open(self.out_file+".tex", "w", encoding='utf-8') as f:
