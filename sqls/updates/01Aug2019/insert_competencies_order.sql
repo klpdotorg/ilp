428e07561f1037dfe3710069b01acf86b743786d
shivangi <shivangi@klp.org.in>
2019-08-02 15:51:45 +0530

    Competency order and map for 1920 inserted

---
+++ sqls/updates/01Aug2019/insert_competencies_order.sql
@@ -0,0 +1,17 @@
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(62,'Number Recognition', 1);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(62,'Place Value', 2);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(62,'Addition', 3);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(62,'Subtraction', 4);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(62,'Multiplication', 5);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(62,'Division', 6);
+
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(63,'Number Recognition', 1);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(63,'Addition', 2);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(63,'Subtraction', 3);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(63,'Multiplication', 4);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(63,'Division', 5);
+
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(64,'Addition', 1);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(64,'Subtraction', 2);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(64,'Multiplication', 3);
+insert into assessments_competencyorder(questiongroup_id,key,sequence) values(64,'Division', 4);
---
+++ sqls/updates/01Aug2019/insert_competencies_questionmap.sql
@@ -0,0 +1,23 @@
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(62,'Number Recognition',925,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(62,'Place Value',927,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(62,'Addition',930,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(62,'Addition',931,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(62,'Subtraction',932,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(62,'Subtraction',933,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(62,'Multiplication',935,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(62,'Division',937,1);
+
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(63,'Number Recognition',945,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(63,'Addition',946,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(63,'Addition',947,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(63,'Subtraction',948,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(63,'Subtraction',949,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(63,'Multiplication',951,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(63,'Division',953,1);
+
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(64,'Addition',967,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(64,'Addition',968,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(64,'Subtraction',969,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(64,'Subtraction',970,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(64,'Multiplication',972,1);
+insert into assessments_competencyquestionmap(questiongroup_id,key,question_id,max_score) values(64,'Division',974,1);
