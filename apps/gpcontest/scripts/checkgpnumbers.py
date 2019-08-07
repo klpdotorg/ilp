c2a24f3090d4931c5a5a9e9e3e532949316ece7a
Subhashini Srinivasan <subha@klp.org.in>
2019-08-07 10:18:56 +0530

    fixes for test scripts

--- apps/gpcontest/scripts/checkgpnumbers.py
+++ apps/gpcontest/scripts/checkgpnumbers.py
@@ -1,7 +1,7 @@
+from apps.gpcontest.test.pandas_gp_numbers_checker import *
+from apps.gpcontest.reports.generate_report import get_gps_for_academic_year
 
 def run():
-        from apps.gpcontest.test.pandas_gp_numbers_checker import *
-        from apps.gpcontest.reports.generate_report import get_gps_for_academic_year
 
         gps = get_gps_for_academic_year(2, 201806, 201903)
         set_db_connection_params("ilp", "klp")
