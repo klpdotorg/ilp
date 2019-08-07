c25c0d1e80fc5cff6c9495d2169c4aee9426453f
Subhashini Srinivasan <subha@klp.org.in>
2019-08-07 11:42:40 +0530

    Fix dates for auto script

--- apps/gpcontest/scripts/test_all_gp_numbers.py
+++ apps/gpcontest/scripts/test_all_gp_numbers.py
@@ -1,9 +1,9 @@
-from pandas_gp_numbers_checker import *
+from gpcontest.scripts.pandas_gp_numbers_checker import *
 from gpcontest.reports.generate_report import get_gps_for_academic_year
 
 
 def run():
-    gps = get_gps_for_academic_year(2, 201806, 201903)
+    gps = get_gps_for_academic_year(2, 201906, 202003)
     set_db_connection_params("ilp", "klp")
     for gp in gps:
         check_gp_numbers(gp)
