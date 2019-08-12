b86d1cb08436b57bc15c2d30770eb644f8a07da6
Subhashini Srinivasan <subha@klp.org.in>
2019-08-12 06:50:45 +0530

    Minor fixes to test script

--- apps/gpcontest/scripts/test_all_gp_numbers.py
+++ apps/gpcontest/scripts/test_all_gp_numbers.py
@@ -1,9 +1,11 @@
-from pandas_gp_numbers_checker import *
+from gpcontest.scripts.pandas_gp_numbers_checker import *
 from gpcontest.reports.generate_report import get_gps_for_academic_year
 
 
 def run():
+    print("Starting check of all GPs..")
     gps = get_gps_for_academic_year(2, 201906, 202003)
     set_db_connection_params("ilp", "klp")
     for gp in gps:
         check_gp_numbers(gp)
+    print("DONE")
