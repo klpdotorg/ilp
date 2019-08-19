6fc1524d77c2d78388bf80c2ed68289cd2c23203
Subhashini Srinivasan <subha@klp.org.in>
2019-08-19 13:47:57 +0530

    minor bug fix related to imports for auth

--- apps/users/views/views.py
+++ apps/users/views/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.contrib.auth.models import Group
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions, status, authentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.response import Response
