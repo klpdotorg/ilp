49c1ff80d168ea500d619163ae8358a21be311c5
shivangi <shivangi@klp.org.in>
2019-09-27 12:34:57 +0530

    Change of GP, new school

---
+++ sqls/updates/27Sept2019.sql
@@ -0,0 +1,5 @@
+update schools_institution set gp_id=5302 where id in (11299,11270,11279,11280,11290,11294);
+update schools_institution set gp_id=2148 where id =43389;
+update schools_institution set gp_id=1591 where id in (6994,6999);
+update schools_institution set gp_id=5039 where id =6686;
+
