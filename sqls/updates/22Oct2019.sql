f690ea4cb004f0671a7e022817ba57ca9da00916
shivangi <shivangi@klp.org.in>
2019-10-22 11:09:05 +0530

    Added one more gp upate

--- sqls/updates/22Oct2019.sql
+++ sqls/updates/22Oct2019.sql
@@ -2,3 +2,4 @@ update schools_institution set gp_id=5290 where id=3612;
 update schools_institution set gp_id=1482 where id=4311;
 update schools_institution set gp_id=1731 where id=3526;
 update schools_institution set gp_id=2807 where id=3667;
+update schools_institution set gp_id=6310 where id=11995;
