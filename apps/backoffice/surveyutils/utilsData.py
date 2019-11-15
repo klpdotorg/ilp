522d10f242af97c6cadf0fadee0c07bef487e965
Subhashini Srinivasan <subha@klp.org.in>
2019-11-15 07:12:33 +0530

    Fixes to filename for data dumps

--- apps/backoffice/surveyutils/utilsData.py
+++ apps/backoffice/surveyutils/utilsData.py
@@ -4,6 +4,7 @@ import csv
 import os
 import calendar
 from assessments.models import Survey, QuestionGroup, QuestionGroup_Questions, Question, AnswerGroup_Institution, AnswerInstitution, SurveyBoundaryAgg
+import shutil
 
 class commonAssessmentDataUtils():
 
@@ -23,11 +24,11 @@ class commonAssessmentDataUtils():
                 questiondata[qs['questiongroup_id']]['questions'][qs['sequence']] = {'question_text':qs['question_question_text'], 'qid': qs['question_id'], 'display_text': qs['question__display_text']}
         return questiondata
 
-    def getQuestionData(self, surveyid):
+    def getQuestionData(self, surveyid, questiongroup_id):
         numquestions = 0
         questiondata = {}
         try:
-            question_qs = QuestionGroup_Questions.objects.filter(questiongroup__survey_id=surveyid).values('questiongroup_id', 'questiongroup__name', 'question__id', 'question__question_text', 'question__display_text','questiongroup__source__name', 'sequence').order_by('sequence')
+            question_qs = QuestionGroup_Questions.objects.filter(questiongroup=questiongroup_id).values('questiongroup_id', 'questiongroup__name', 'question__id', 'question__question_text', 'question__display_text','questiongroup__source__name', 'sequence').order_by('sequence')
         except QuestionGroup_Questions.DoesNotExist:
             print("Did not find relevant question data for surveyid: "+str(surveyid))
             return None
@@ -41,8 +42,37 @@ class commonAssessmentDataUtils():
                 questiondata[qs['questiongroup_id']] = {'name':qs['questiongroup__name'],'source':qs['questiongroup__source__name'], 'questions':[]}
                 questiondata[qs['questiongroup_id']]['questions'].append({'question_text':qs['question__question_text'], 'qid': qs['question__id'], 'display_text': qs['question__display_text'], 'sequence': qs['sequence']})
         return questiondata, numquestions
+    #  def getQuestionData(self, surveyid):
+    #     numquestions = 0
+    #     questiondata = {}
+    #     try:
+    #         question_qs = QuestionGroup_Questions.objects.filter(questiongroup__survey_id=surveyid).values('questiongroup_id', 'questiongroup__name', 'question__id', 'question__question_text', 'question__display_text','questiongroup__source__name', 'sequence').order_by('sequence')
+    #     except QuestionGroup_Questions.DoesNotExist:
+    #         print("Did not find relevant question data for surveyid: "+str(surveyid))
+    #         return None
 
+    #     for qs in question_qs:
+    #         if numquestions < qs['sequence']:
+    #             numquestions = numquestions+1
+    #         if qs['questiongroup_id'] in questiondata:
+    #             questiondata[qs['questiongroup_id']]['questions'].append({'question_text':qs['question__question_text'], 'qid': qs['question__id'], 'display_text': qs['question__display_text'], 'sequence': qs['sequence']})
+    #         else:
+    #             questiondata[qs['questiongroup_id']] = {'name':qs['questiongroup__name'],'source':qs['questiongroup__source__name'], 'questions':[]}
+    #             questiondata[qs['questiongroup_id']]['questions'].append({'question_text':qs['question__question_text'], 'qid': qs['question__id'], 'display_text': qs['question__display_text'], 'sequence': qs['sequence']})
+    #     return questiondata, numquestions
 
+    def getQuestionGroups(self, surveyid, from_yearmonth, to_yearmonth):
+        list_questiongroups = []
+        if from_yearmonth is None and to_yearmonth is None:
+            list_questiongroups = QuestionGroup.objects.filter(survey_id=surveyid).distinct('id').values_list('id', flat=True)
+        else:
+            from_short_year = from_yearmonth[2:-2]
+            to_short_year = int(to_yearmonth[2:-2])
+            if int(to_short_year) == int(from_short_year):
+                to_short_year = int(to_short_year) + 1
+            academic_year_id = from_short_year + str(to_short_year)
+            list_questiongroups = QuestionGroup.objects.filter(survey_id=surveyid).filter(academic_year_id=academic_year_id).distinct('id').values_list('id', flat=True)
+        return list_questiongroups
 
     def validateSurvey(self, surveyid):
         if surveyid == None:
@@ -55,47 +85,84 @@ class commonAssessmentDataUtils():
             return None
         return survey
 
+    def create_file_name(self, filename, from_yearmonth, to_yearmonth, qgroup_id, qgroup_name):
+        ''' Construct a filename with a questiongroup ID, from, to dates and timestamp '''
+        now = date.today()
+        filename = filename + "_" + str(qgroup_id) + "_" + qgroup_name \
+            + "_" + str(from_yearmonth) + "_" + str(to_yearmonth) + "_" + str(now)
+        return filename 
 
-    def createXLS(self, surveyinfo, questioninfo, numquestions, assessmentdata, filename, skipxls):
-        csvfile = filename+".csv"
-        xlsfile = filename+".xlsx"
-        book = xlwt.Workbook()
-        sheet = book.add_sheet("AssessmentData")
-        with open(csvfile, mode='w') as datafile:
-            filewriter = csv.writer(datafile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            row = ['State', 'District','Block','Cluster','GPName', 'GP Id','SchoolName','SchoolId','DiseCode','QuestionGroup Id','QuestionGroupName', 'Source Name', 'Date of Visit', 'Academic Year of Visit', 'Group Value', 'RespondentType', 'UserType','UserMobileNumber']
-            for i in range(1, numquestions+1):
-                row = row+['QuestionText_'+str(i),'Answer_'+str(i)]
-            filewriter.writerow(row)
-            for state in assessmentdata:
-                for district in assessmentdata[state]:
-                    for block in assessmentdata[state][district]:
-                        for schoolid in assessmentdata[state][district][block]:
-                            schooldata = assessmentdata[state][district][block][schoolid]
-                            for qgid in schooldata["questiongroups"]:
-                                for agid in schooldata["questiongroups"][qgid]["answergroups"]:
-                                    answergroup = schooldata["questiongroups"][qgid]["answergroups"][agid]
-                                    row = [state,district,block,schooldata["cluster"],schooldata["gpname"],schooldata["gpid"],schooldata["schoolname"],schoolid,schooldata["disecode"],qgid,questioninfo[qgid]["name"],questioninfo[qgid]["source"],answergroup["dateofvisit"],answergroup["academicyear"],answergroup["groupvalue"],answergroup["respondenttype"],answergroup["usertype"],answergroup["usermobilenumber"]]
-                                    for answer in answergroup["answers"]:
-                                        row = row+[answer["questiontext"],answer["answer"]]
-                                    filewriter.writerow(row)
+    def createXLS(self, surveyinfo, questioninfo, numquestions, questiongroup_id, assessmentdata, filename, skipxls):
+        try:
+            csvfile = filename+".csv"
+            xlsfile = filename+".xlsx"
+            book = xlwt.Workbook()
+            sheet = book.add_sheet("AssessmentData")
+            with open(csvfile, mode='w') as datafile:
+                filewriter = csv.writer(datafile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                row = ['State', 'District','Block','Cluster','GPName', 'GP Id','SchoolName','SchoolId','DiseCode','QuestionGroup Id','QuestionGroupName', 'Source Name', 'Date of Visit', 'Academic Year of Visit', 'Group Value', 'RespondentType', 'UserType','UserMobileNumber']
+                #for i in range(1, numquestions+1):
+                    #row = row+['QuestionText_'+str(i),'Answer_'+str(i)]
+                questions_list = questioninfo[questiongroup_id]["questions"]
+                for question in questions_list:
+                    qn_text = question["display_text"]
+                    row = row + [qn_text]
+                filewriter.writerow(row)
+                for state in assessmentdata:
+                    for district in assessmentdata[state]:
+                        for block in assessmentdata[state][district]:
+                            for schoolid in assessmentdata[state][district][block]:
+                                schooldata = assessmentdata[state][district][block][schoolid]
+                                for qgid in schooldata["questiongroups"]:
+                                    for agid in schooldata["questiongroups"][qgid]["answergroups"]:
+                                        answergroup = schooldata["questiongroups"][qgid]["answergroups"][agid]
+                                        row = [state,district,block,schooldata["cluster"],schooldata["gpname"],schooldata["gpid"],schooldata["schoolname"],schoolid,schooldata["disecode"],qgid,questioninfo[qgid]["name"],questioninfo[qgid]["source"],answergroup["dateofvisit"],answergroup["academicyear"],answergroup["groupvalue"],answergroup["respondenttype"],answergroup["usertype"],answergroup["usermobilenumber"]]
+                                        for answer in answergroup["answers"]:
+                                            # row = row+[answer["questiontext"],answer["answer"]]
+                                            row = row+[answer["answer"]]
+
+                                        filewriter.writerow(row)
+                datafile.close()
+        except Exception as e:
             datafile.close()
-        # if skipxls is True, then no need to create the XLS file. Stop with CSV
-        if skipxls is False:
-            # For very big files, convert CSV to XLS. Changed from xlwt package
-            # to pandas because xlwt was failing to write rows > 65536
-            import pandas as pd
-            data = pd.read_csv(csvfile, low_memory=False)
-            writer = pd.ExcelWriter(xlsfile, engine='xlsxwriter')
-            data.to_excel(writer, 'AssessmentData')
-            writer.save()
-            # Old code using xlwt package to write xls file
-            # with open(csvfile, 'rt', encoding='utf8') as f:
-            #     reader = csv.reader(f)
-            #     for r, row in enumerate(reader):
-            #         for c, col in enumerate(row):
-            #             sheet.write(r, c, col)
-            # book.save(xlsfile)
+            import pdb; pdb.set_trace()
+            print("Exception while creating CSV")
+        else:
+            # Create XLS files if required
+            if not skipxls:
+                import pandas as pd
+                data = pd.read_csv(csvfile, low_memory=False)
+                writer = pd.ExcelWriter(xlsfile, engine='xlsxwriter')
+                data.to_excel(writer, 'AssessmentData')
+                writer.save()
+            # Move the CSV & XLS files to the proper location
+            foldername = surveyinfo.name.replace(" ", "")
+            parent_dir = "generated_files"
+            folderpath = os.path.join(parent_dir,foldername)
+            # Move the files
+            if not os.path.exists(parent_dir):
+                os.mkdir(parent_dir)
+            if not os.path.exists(folderpath):
+                os.mkdir(folderpath)
+            shutil.move(csvfile, folderpath + "/" + csvfile)
+
+            if not skipxls: # Move the XLS file if it was created
+                shutil.move(xlsfile, folderpath + "/" + xlsfile)    
+
+        # For very big files, convert CSV to XLS. Changed from xlwt package
+        # to pandas because xlwt was failing to write rows > 65536
+        # import pandas as pd
+        # data = pd.read_csv(csvfile, low_memory=False)
+        # writer = pd.ExcelWriter(xlsfile, engine='xlsxwriter')
+        # data.to_excel(writer, 'AssessmentData')
+        # writer.save()
+        # Old code using xlwt package to write xls file
+        # with open(csvfile, 'rt', encoding='utf8') as f:
+        #     reader = csv.reader(f)
+        #     for r, row in enumerate(reader):
+        #         for c, col in enumerate(row):
+        #             sheet.write(r, c, col)
+        # book.save(xlsfile)
 
     def deleteTempFiles(self, tempFiles):
         for f in tempFiles:
@@ -123,10 +190,9 @@ class commonAssessmentDataUtils():
             to_date = date(year=to_year, month=to_month, day=calendar.monthrange(to_year, to_month)[1])
         return from_date, to_date
 
-    def getAssessmentData(self, surveyinfo, questioninfo, from_yearmonth, to_yearmonth):
-        assessmentdata = {}
+    def dumpData(self, surveyinfo, from_yearmonth, to_yearmonth, filename, skipxls):
+        questiongroups_list = self.getQuestionGroups(surveyinfo.id, from_yearmonth, to_yearmonth)
         if from_yearmonth and to_yearmonth is not None:
-            from_date, to_date = self.convertToDate(from_yearmonth, to_yearmonth)
             districts = SurveyBoundaryAgg.objects.filter(
                 survey_id=surveyinfo.id, boundary_id__boundary_type_id='SD').filter(
                     yearmonth__range=[from_yearmonth, to_yearmonth]
@@ -134,12 +200,24 @@ class commonAssessmentDataUtils():
         else:
             districts = SurveyBoundaryAgg.objects.filter(
                 survey_id=surveyinfo.id, boundary_id__boundary_type_id='SD').values_list('boundary_id', flat=True).distinct()
-        print(districts)
+        for questiongroup in questiongroups_list:
+            q_group = QuestionGroup.objects.get(id=questiongroup)
+            #strip out spaces
+            q_group_name = q_group.name.replace(" ", "")
+            # We are getting assessment data per questiongroup and creating XLS files per questiongroup
+            assessment_data, numquestions, questioninfo = self.getAssessmentData(surveyinfo, districts, questiongroup, from_yearmonth, to_yearmonth)
+            # Create the CSV/XLS file name so it is descriptive/neat
+            output_file_name = self.create_file_name(filename,from_yearmonth, to_yearmonth,questiongroup, q_group_name)
+            # Create the CSV/XLS files
+            self.createXLS(surveyinfo, questioninfo, numquestions, questiongroup, assessment_data, output_file_name, skipxls)
+
+
+    def getAssessmentData(self, surveyinfo, districts, questiongroup, from_yearmonth, to_yearmonth):
+        from_date, to_date = self.convertToDate(from_yearmonth, to_yearmonth)
+        assessmentdata = {}
+        questioninfo, numquestions = self.getQuestionData(surveyinfo.id,questiongroup)
         for district in districts:
-            print(district)
-            answergroups = AnswerGroup_Institution.objects.filter(institution__admin1__id=district, questiongroup__survey_id=surveyinfo.id)
-            # If from and to dates are given filter based on that
-           
+            answergroups = AnswerGroup_Institution.objects.filter(institution__admin1__id=district, questiongroup=questiongroup)               
             if from_date is not None:
                 answergroups = answergroups.filter(date_of_visit__gte=from_date)
             if to_date is not None:
@@ -181,11 +259,7 @@ class commonAssessmentDataUtils():
                                     assessmentdata[state][district][block][schoolid]["questiongroups"][qgid]={"answergroups":{agid:{"dateofvisit":dateofvisit,"academicyear":academicyear,"groupvalue":groupvalue,"respondenttype":respondenttype,"usertype":usertype,"usermobilenumber":usermobilenumber,"answers":[]}}}
                                 else:
                                     assessmentdata[state][district][block][schoolid]["questiongroups"][qgid]["answergroups"][agid]={"dateofvisit":dateofvisit,"academicyear":academicyear,"groupvalue":groupvalue,"respondenttype":respondenttype,"usertype":usertype,"usermobilenumber":usermobilenumber,"answers":[]}
-
-
-
                 answers = AnswerInstitution.objects.filter(answergroup=agid).values('question__id','answer')
-
                 for question in questioninfo[qgid]["questions"]:
                     found=0
                     for answer in answers:
@@ -195,6 +269,6 @@ class commonAssessmentDataUtils():
                             break
                     if found == 0:
                         assessmentdata[state][district][block][schoolid]["questiongroups"][qgid]["answergroups"][agid]["answers"].append({'questiontext':question['question_text'],'answer':''})
-            print("Finished putting in data")
-        return assessmentdata
- 
+            #self.createXLS(surveyinfo, questioninfo, numquestions, assessmentdata, filename+ "_" + str(q_group_name))
+        print("Finished putting in data")
+        return assessmentdata, numquestions, questioninfo
