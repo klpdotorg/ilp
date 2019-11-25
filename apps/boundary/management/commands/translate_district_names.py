054bd5c72c50f5c7ee1f5ff0e60e3d580d3da2ff
Subhashini Srinivasan <subha@klp.org.in>
2019-11-25 11:47:03 +0530

    Fix

--- apps/boundary/management/commands/translate_district_names.py
+++ apps/boundary/management/commands/translate_district_names.py
@@ -17,25 +17,25 @@ from fuzzywuzzy import process
 class Command(BaseCommand):
     def handle(self, *args, **options):
         rdpr_districts = pandas.read_csv('apps/boundary/management/commands/csvs/KA_boundaries/KA_unique_districts_all_translations.csv')
-    print(rdpr_districts)
-    cursor = connection.cursor()
-    df = pandas.read_sql_query('select id,name from boundary_boundary where parent_id=2 and boundary_type_id=\'SD\'',con=connection)
-    df['lang_name'] = ""
-    print(df)
-    db_district_names = df["name"]
-    print(db_district_names)
-    for index, row in df.iterrows():
-        result = process.extractOne(row['name'], rdpr_districts['english_name'])
-        # Picking 85 as the boundary for acceptable translations because
-        # for most regional names this works. Any output of this script
-        # needs to get reviewed anyway for accuracy.
-        if result[1] > 85:
-            # print("For %s, the BEST match is: %s " % (row['name'],result))
-            df.at[index,'lang_name'] = rdpr_districts['lang_name'][result[2]]
-            sql = "UPDATE boundary_boundary SET lang_name=\'{0}\' WHERE id={1}".format(rdpr_districts['lang_name'][result[2]], row['id'])
-            cursor.execute(sql)
-        else:
-            print("For %s, the NEAREST match is: %s " % (row['name'],result))
-    conn.commit()
-    result_df = pandas.read_sql_query('select id,name, lang_name from boundary_boundary where parent_id=2 and boundary_type_id=\'SD\'',con=connection)
-    print(result_df)
+        print(rdpr_districts)
+        cursor = connection.cursor()
+        df = pandas.read_sql_query('select id,name from boundary_boundary where parent_id=2 and boundary_type_id=\'SD\'',con=connection)
+        df['lang_name'] = ""
+        print(df)
+        db_district_names = df["name"]
+        print(db_district_names)
+        for index, row in df.iterrows():
+            result = process.extractOne(row['name'], rdpr_districts['english_name'])
+            # Picking 85 as the boundary for acceptable translations because
+            # for most regional names this works. Any output of this script
+            # needs to get reviewed anyway for accuracy.
+            if result[1] > 85:
+                # print("For %s, the BEST match is: %s " % (row['name'],result))
+                df.at[index,'lang_name'] = rdpr_districts['lang_name'][result[2]]
+                sql = "UPDATE boundary_boundary SET lang_name=\'{0}\' WHERE id={1}".format(rdpr_districts['lang_name'][result[2]], row['id'])
+                cursor.execute(sql)
+            else:
+                print("For %s, the NEAREST match is: %s " % (row['name'],result))
+        conn.commit()
+        result_df = pandas.read_sql_query('select id,name, lang_name from boundary_boundary where parent_id=2 and boundary_type_id=\'SD\'',con=connection)
+        print(result_df)
