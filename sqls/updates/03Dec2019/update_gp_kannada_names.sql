a498d73be41183b5146abb13585b139b7bc3748b
Subhashini Srinivasan <subha@klp.org.in>
2019-12-03 11:10:50 +0530

    fixes for gp names updates

--- sqls/updates/03Dec2019/update_gp_kannada_names.sql
+++ sqls/updates/03Dec2019/update_gp_kannada_names.sql
@@ -1,21 +1,21 @@
-'''Chincholi tp'''
+/*Chincholi tp'*/
 UPDATE boundary_electionboundary SET const_ward_name='chincholi tp' WHERE id=1641;
 UPDATE boundary_electionboundary SET const_ward_lang_name='ಚಿಂಚೊಳ್ಳಿ(ಎಚ್)' WHERE id=1641;
-'''7th Hosakote'''
+/*7th Hosakote*/
 UPDATE boundary_electionboundary SET const_ward_lang_name='7 ನೇ ಹೊಸಕೋಟೆ' WHERE id=2793;
-'''K g halli'''
+/*K g halli*/
 UPDATE boundary_electionboundary SET const_ward_name='K g halli' WHERE id=3103;
 UPDATE boundary_electionboundary SET const_ward_lang_name='ಕೆ.ಜಿ.ಹಳ್ಳಿ' WHERE id=3103;
-'''Pattana panchayathi arakalagu'''
+/*Pattana panchayathi arakalagu*/
 UPDATE boundary_electionboundary SET const_ward_name='pattana panchayathi arakalagu' WHERE id=4978;
 UPDATE boundary_electionboundary SET const_ward_lang_name='ಪಟ್ಟಣ' WHERE id=4978;
-'''Sirsi (a)'''
+/*Sirsi (a)*/
 UPDATE boundary_electionboundary SET const_ward_name='Sirsi (a)' WHERE id=5440;
 UPDATE boundary_electionboundary SET const_ward_lang_name='ಸಿರ್ಸಿ (ಎ)' WHERE id=5440;
-'''Y n hosakote'''
+/*Y n hosakote*/
 UPDATE boundary_electionboundary SET const_ward_name='Y n hosakote' WHERE id=6033;
 UPDATE boundary_electionboundary SET const_ward_lang_name='ವ್ಐ.ಎನ್. ಹೊಸಕೋಟೆ' WHERE id=6033;
-'''Gorabal'''
+/*Gorabal*/
 UPDATE boundary_electionboundary SET const_ward_lang_name='ಗೊರಬಾಳ' WHERE id=6307;
-'''Bedasagau'''
+/*Bedasagau*/
 UPDATE boundary_electionboundary SET const_ward_lang_name='ಭೆಡಸೂರ' WHERE id=6396;
