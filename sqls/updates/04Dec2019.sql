fa1106882c677180517e00760ae89ee843535b26
Subhashini Srinivasan <subha@klp.org.in>
2019-12-04 11:13:10 +0530

    Fix chikmagalur block name

--- sqls/updates/04Dec2019.sql
+++ sqls/updates/04Dec2019.sql
@@ -3,3 +3,5 @@
 UPDATE boundary_boundary set lang_name='ಮಂಡ್ಯ ಉತ್ತರ' where id=595;
 -- Mandya South
 UPDATE boundary_boundary set lang_name='ಮಂಡ್ಯ ದಕ್ಷಿಣ' where id=591;
+--chikmagalur block has messed up Kannada name. Fix that
+UPDATE boundary_boundary set lang_name='ಚಿಕ್ಕಮಗಳೂರು' where id=564;
