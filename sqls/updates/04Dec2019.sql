cb045b72b75cc535ceb2e9ed4d76a280d4a222ee
Subhashini Srinivasan <subha@klp.org.in>
2019-12-04 10:36:36 +0530

    Update for block names issue in mandya

---
+++ sqls/updates/04Dec2019.sql
@@ -0,0 +1,5 @@
+-- Update to be run for Manday North and Manday South language names (blocks)
+-- Mandya North
+UPDATE boundary_boundary set lang_name='ಮಂಡ್ಯ ಉತ್ತರ' where id=595;
+-- Mandya South
+UPDATE boundary_boundary set lang_name='ಮಂಡ್ಯ ದಕ್ಷಿಣ' where id=591;
