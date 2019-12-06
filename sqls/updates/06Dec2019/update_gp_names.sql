18a0f47f7170379ba612c2c91d0c2a9122bfaace
Subhashini Srinivasan <subha@klp.org.in>
2019-12-06 12:50:46 +0530

    new cmd to update gp_translations

---
+++ sqls/updates/06Dec2019/update_gp_names.sql
@@ -0,0 +1,22 @@
+/*
+UpDATE GP NAMES FROM WHICH BRACKETS WERE REMOVED FOR LOTS 16 & 17
+*/
+UPDATE boundary_electionboundary SET const_ward_name = 'Wadgaon(D)' WHERE id=6014;
+UPDATE boundary_electionboundary SET const_ward_name = 'Talwad(K)' WHERE id=5627;
+UPDATE boundary_electionboundary SET const_ward_name = 'Sirsi(A)' WHERE id=5440;
+UPDATE boundary_electionboundary SET const_ward_name = 'Nittur(B)' WHERE id=4875;
+UPDATE boundary_electionboundary SET const_ward_name = 'Mudhol(B)' WHERE id=4507;
+UPDATE boundary_electionboundary SET const_ward_name = 'Koutha(B)' WHERE id=3814;
+UPDATE boundary_electionboundary SET const_ward_name = 'Kolar(K)' WHERE id=3695;
+UPDATE boundary_electionboundary SET const_ward_name = 'Kaplapur(A)' WHERE id=3411;
+UPDATE boundary_electionboundary SET const_ward_name = 'Chikli(J)' WHERE id=1618;
+UPDATE boundary_electionboundary SET const_ward_name = 'Belkuni(CH)' WHERE id=1186;
+UPDATE boundary_electionboundary SET const_ward_name = 'Beeri(K)' WHERE id=1128;
+UPDATE boundary_electionboundary SET const_ward_name = 'Dhannura(K)' WHERE id=1824;
+UPDATE boundary_electionboundary SET const_ward_name = 'Hallikheda(K)' WHERE id=2361;
+
+
+
+
+
+
