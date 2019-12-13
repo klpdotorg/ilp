e8b496089fccf729b5fc63e1203bda5915fea88f
Subhashini Srinivasan <subha@klp.org.in>
2019-12-13 10:02:57 +0530

    lot 18 gp name updates

---
+++ sqls/updates/12Dec2019/lot15_gp_translations_update.sql
@@ -0,0 +1,1 @@
+UPDATE boundary_electionboundary SET const_ward_lang_name = 'ನೆಲಕುದ್ರಿ' where id=4798;
---
+++ sqls/updates/12Dec2019/lot18_gp_translations_update.sql
@@ -0,0 +1,12 @@
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಚೋಳೂರು' WHERE id=1679;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ದಪ್ಪರ್ತಿ' WHERE id=1737;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ದರಗಾಜೋಗಹಳ್ಳಿ' WHERE id=1742;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ದಾನಗೆರೆ' WHERE id=1821;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ದೊಡ್ಡನಲ್ಲಾಳ ' WHERE id=1872;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ದೊಡ್ಡಬಳ್ಳಾಪುರ ಟೌನ್ ' WHERE id=1880;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಗದರೆ' WHERE id=2011;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಹಿರೇಕೆರೆಹಳ್ಳಿ' WHERE id=2645;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಸಕ್ಕರೆಗೊಲ್ಲಹಳ್ಳಿ' WHERE id=5163;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಸಮೇತ್ತನಹಳ್ಳಿ' WHERE id=5185;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಸೋಮೇನಹಳ್ಳಿ' WHERE id=5465;
+
