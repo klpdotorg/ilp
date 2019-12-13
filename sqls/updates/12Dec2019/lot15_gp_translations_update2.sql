f3cc9824644190b17c3373aac3e6c1727ffa3ca2
Subhashini Srinivasan <subha@klp.org.in>
2019-12-13 11:18:08 +0530

    Second set of translations for Lot 15

---
+++ sqls/updates/12Dec2019/lot15_gp_translations_update2.sql
@@ -0,0 +1,9 @@
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಚಾನ್ನಾಳು' WHERE id=1482;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ದಾನನಾಯಕನಕೆರೆ' WHERE id=1731;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ದಾನಪುರ' WHERE id=1733;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಕರೇಕಲ್‌' WHERE id=3445;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಒರ್ವಾಯಿ' WHERE id=4907;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಯರಂಗುಡಿ' WHERE id=6153;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಪೊಲನಾಯಕನಹಳ್ಳಿ' WHERE id=6178;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಕಲಕೇರಿ' WHERE id=6297;
+
