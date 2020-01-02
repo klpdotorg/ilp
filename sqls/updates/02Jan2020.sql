3679892daeb790d9811a169654471cd635886ec9
shivangi <shivangi@klp.org.in>
2020-01-02 08:04:31 +0530

    gp changes

---
+++ sqls/updates/02Jan2020.sql
@@ -0,0 +1,7 @@
+update schools_institution set gp_id=4621 where id=12287;
+update schools_institution set gp_id=5257 where id in (37656, 37655,37657);
+update schools_institution set gp_id=3813 where id=47797;
+update schools_institution set gp_id=4444 where id=43390;
+update schools_institution set gp_id=4037 where id=43389;
+update schools_institution set gp_id=5325 where id=7056;
+update schools_institution set gp_id=5325 where id=43383;
