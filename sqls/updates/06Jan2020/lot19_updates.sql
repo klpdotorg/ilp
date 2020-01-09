34b50cba75eb3607f65fccf21cf8fd59608f636b
Subhashini Srinivasan <subha@klp.org.in>
2020-01-09 10:02:56 +0530

    gp name updates for lot 19

--- sqls/updates/06Jan2020/lot19_updates.sql
+++ sqls/updates/06Jan2020/lot19_updates.sql
@@ -1,3 +1,16 @@
 UPDATE schools_institution SET gp_id=6665 where id=115117;
 UPDATE boundary_electionboundary SET const_ward_name='Barkani (C)' where id=2003;
 UPDATE boundary_electionboundary SET const_ward_lang_name='ಬರ್ಕನಿ (ಸಿ)' where id=2003;
+-- GP names updates
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಅಡನೂರು' where id=515;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಅಥರ್ಗಾ' where id=826;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಬ್ಯಾಗವಾಟ' where id=1431;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಚಿಂಚರ್ಕಿ' where id=1640;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಡಿ. ಯಸ್. ಹಡಗಲಿ' where id=1727;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಗಣದಿನ್ನಿ' where id=2037;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಹೆಚ್. ಡಿ. ಪುರ' where id=2527;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಕೆ ಗುಡದಿನ್ನಿ' where id=3120;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಕುರುಡಿ' where id=3923;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ನಕ್ಕುಂದಿ' where id=4684;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ತೋರಣದಿನ್ನಿ' where id=5796;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಉಗಣಿಕೇರಿ' where id=5876;
