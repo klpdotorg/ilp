5e32acf1be34f1b81bb521738f0700154ebf29f7
Subhashini Srinivasan <subha@klp.org.in>
2020-01-13 11:38:20 +0530

    Fix GP names for Lot 19

---
+++ sqls/updates/13Jan2020/update_lot19_gpnames.sql
@@ -0,0 +1,4 @@
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಡಂಬಾಳ್ ಎಸ್ ಹಡಗಲಿ' where id=1727;
+UPDATE boundary_electionboundary SET const_ward_name='H.D.Pura' where id=2527;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಕೆ.ಆರ್. ಹಳ್ಳಿ' where id=3125;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ನಕ್ಕುಂದ' where id=4684;
