365c37b1c193a122899cf84ef6d96d4bcfa150a1
Subhashini Srinivasan <subha@klp.org.in>
2020-01-22 22:52:05 +0530

    GP lot 20 names updates

---
+++ sqls/updates/22Jan2020/lot20_gpnames_updates.sql
@@ -0,0 +1,4 @@
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಭೂತಲದಿನ್ನಿ' WHERE id=1423;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಈವೂರ' WHERE id=1992;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಅರ, ಹೆಚ್.ಕ್ಯಾಂಪ್‌' WHERE id=5034;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಶಲವಡಿ' WHERE id=5301;
