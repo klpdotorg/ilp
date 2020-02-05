a8fffcb45e1de43cf1aeaf46c30b54524d65240c
Subhashini Srinivasan <subha@klp.org.in>
2020-02-05 10:43:44 +0530

    lot 21 gp names fixes

---
+++ sqls/updates/05Feb2020/lot21_gpnames_fixes.sql
@@ -0,0 +1,11 @@
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಮಡಕಲ್' WHERE id=4107;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಬರುಆನ್ (ಕೆ)' WHERE id=5005;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಜುಮಲಾಪುರ' WHERE id=3092;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಮದ್ದರ್ಕಿ' WHERE id=4086;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ವಿ.ವಿ ಪುರ' WHERE id=5931;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಬಾದರ್ಲಿ' WHERE id=910;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಬಟಗೇರಾ (ಬಿ)' WHERE id=1101;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಉತಕನೂರ್' WHERE id=5921;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಹಿರೇಕೊಟ್ನೆಕಲ್' WHERE id=2655;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಕೋಡ್ಲಾ' WHERE id=3665;
+
