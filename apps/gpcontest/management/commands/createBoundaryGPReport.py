2b545e46eb24a26004a6aa17fcc08580add37b6a
shivangi <shivangi@klp.org.in>
2020-02-05 08:27:27 +0530

    lang a class parameter

--- apps/gpcontest/management/commands/createBoundaryGPReport.py
+++ apps/gpcontest/management/commands/createBoundaryGPReport.py
@@ -62,6 +62,7 @@ class Command(BaseCommand, baseReport.CommonUtils):
     colour = "bw"
     imagesdir = basefiledir+"/apps/gpcontest/images/"
     translatedmonth = {1:'ಜನವರಿ',2:'ಫೆಬ್ರವರಿ',3:'ಮಾರ್ಚ್',4:'ಎಪ್ರಿಲ್',5:'ಮೇ',6:'ಜೂನ್',7:'ಜುಲೈ',8:'ಆಗಸ್ಟ್',9:'ಸೆಪ್ಟಂಬರ್',10:'ಅಕ್ಟೋಬರ್',11:'ನವೆಂಬರ್',12:'ಡಿಸೆಂಬರ್'}
+    language = 'kannada'
 
     def add_arguments(self, parser):
         parser.add_argument('surveyid')
@@ -340,8 +341,8 @@ class Command(BaseCommand, baseReport.CommonUtils):
         self.onlydistrict = options.get("onlydistrict", False)
         blockids = options.get("blockid", None)
 
-        language = options.get("lang")
-        self.templatedir = self.templatedir+"/"+language+"/"
+        self.language = options.get("lang")
+        self.templatedir = self.templatedir+"/"+self.language+"/"
         self.imagesdir = self.imagesdir+"/"+self.language+"/"
 
         reportcolour = options.get("reportcolour")
