45d17a600bf4b0350b95e061810f50c2359fc823
Subhashini Srinivasan <subha@klp.org.in>
2020-02-10 11:48:57 +0530

    Correcting mistake with the 07Feb2020 update

--- sqls/updates/07Feb2020/updates_to_gps.sql
+++ sqls/updates/07Feb2020/updates_to_gps.sql
@@ -1,6 +1,6 @@
 update assessments_answergroup_institution set date_of_visit='2019-08-22'::timestamp::date where questiongroup_id in (select id from assessments_questiongroup where survey_id=2) and institution_id in (select id from schools_institution where gp_id =5017)  and to_char(date_of_visit,'YYYYMM') = '201909';
 update assessments_answergroup_institution set date_of_visit='2019-08-21'::timestamp::date where questiongroup_id in (select id from assessments_questiongroup where survey_id=2) and institution_id in (select id from schools_institution where gp_id =5741)  and to_char(date_of_visit,'YYYYMM') = '201909';
-update assessments_answergroup_institution set date_of_visit='2019-12-12'::timestamp::date where questiongroup_id in (select id from assessments_questiongroup where survey_id=2) and institution_id in (select id from schools_institution where gp_id =4037)  and to_char(date_of_visit,'YYYYMM') = '201912';
+update assessments_answergroup_institution set date_of_visit='2019-12-12'::timestamp::date where questiongroup_id in (select id from assessments_questiongroup where survey_id=2) and institution_id in (select id from schools_institution where gp_id =4037)  and to_char(date_of_visit,'YYYYMM') = '201909';
 
 --Lot 21 gp names updates
 UPDATE boundary_electionboundary SET const_ward_lang_name='ಮುಖಹಳ್ಲಿ' WHERE id=4533;
