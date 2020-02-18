ab7f6850588fa5d23072ff3c55838ffb481da41c
Subhashini Srinivasan <subha@klp.org.in>
2020-02-18 11:55:53 +0530

    GP names updates for lot 23 and 2194

---
+++ sqls/updates/18Feb2020/lot23_gpnames_update.sql
@@ -0,0 +1,10 @@
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಅರಳಗುಂಡಗಿ' WHERE id=766;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಬಿರಾಳ.ಬಿ' WHERE id=1365;
+UPDATE boundary_electionboundary SET const_ward_name='kallur' WHERE id=1762;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಹಸಿರುವಳ್ಳಿ' WHERE id=2506;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಶ್ರೀನಿವಾಸಪುರ' WHERE id=5505;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ನರಸೀಪುರ' WHERE id=6428;
+
+
+
+
