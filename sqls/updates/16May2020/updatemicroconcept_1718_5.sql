da06e6da39dd4dc49dea6a5910e817c8581f6cff
shivangi <shivangi@klp.org.in>
2020-03-16 07:53:55 +0530

    question change for division

---
+++ sqls/updates/16May2020/updatemicroconcept_1718_5.sql
@@ -0,0 +1,1 @@
+update assessments_question set microconcept_id='long_division_with_remainder', display_text='Long Division with Remainder', question_text='Long Division without Remainder'  where id=777;      
