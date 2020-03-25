eb48426512d96de909e618feaa7e75197a0cec4f
Subhashini Srinivasan <subha@klp.org.in>
2020-03-25 20:30:13 +0530

    lot29 gp names updates

---
+++ sqls/updates/25Mar2020/lot29_gpnames_updates.sql
@@ -0,0 +1,5 @@
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಮರಟುರಾ' WHERE id=4336;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಪಸಪುಲ್' WHERE id=4967;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಎಲ್ಲೇರಿ' WHERE id=6083;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ನಾರಾಯಣಪುರ' WHERE id=6417;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಕಲ್ಲಹಂಗರಗ' WHERE id=213009038;
