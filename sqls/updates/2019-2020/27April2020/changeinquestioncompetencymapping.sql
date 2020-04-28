0d6155e4995f3b422cda8b38b4571f1d44420c05
shivangi <shivangi@klp.org.in>
2020-04-28 11:02:17 +0530

    changed question mapping for gpcontest

---
+++ sqls/updates/2019-2020/27April2020/changeinquestioncompetencymapping.sql
@@ -0,0 +1,10 @@
+update assessments_competencyquestionmap set question_id=281 where question_id=280 and questiongroup_id=21;
+update assessments_competencyquestionmap set question_id=281 where question_id=776 and questiongroup_id=22;
+delete from assessments_competencyquestionmap where questiongroup_id=23 and key='Number Recognition';
+update assessments_competencyquestionmap set question_id=781 where question_id=780 and questiongroup_id=23;
+update assessments_competencyquestionmap set question_id=728 where question_id=719 and questiongroup_id=45;
+delete from assessments_competencyquestionmap where questiongroup_id=47 and key='Number Recognition';
+update assessments_competencyquestionmap set question_id=934 where question_id=935 and questiongroup_id=62;
+update assessments_competencyquestionmap set question_id=950 where question_id=951 and questiongroup_id=63;
+update assessments_competencyquestionmap set question_id=971 where question_id=972 and questiongroup_id=64;
+
