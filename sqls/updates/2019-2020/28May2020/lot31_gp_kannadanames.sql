d121af140ee73b9cc553a12bf43cbb9982e8faaa
Subhashini Srinivasan <subha@klp.org.in>
2020-05-30 09:40:23 +0530

    lot 31 kannada names

---
+++ sqls/updates/2019-2020/28May2020/lot31_gp_kannadanames.sql
@@ -0,0 +1,3 @@
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಅರಕೇರಾ (ಬಿ)' WHERE id=802;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಮಾಧವಾರ' WHERE id=4100;
+UPDATE boundary_electionboundary SET const_ward_lang_name='ಕೊಂಕಲ್‌' WHERE id=6309;
