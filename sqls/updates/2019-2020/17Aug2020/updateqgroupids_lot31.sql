594801c758d87a6971222ae1fe3c281e1b9a2cb5
Subhashini Srinivasan <subha@klp.org.in>
2020-08-17 17:38:38 +0530

    Lot 31 fixes

---
+++ sqls/updates/2019-2020/17Aug2020/updateqgroupids_lot31.sql
@@ -0,0 +1,3 @@
+UPDATE assessments_answergroup_institution SET questiongroup_id=62 WHERE questiongroup_id=45 and date_of_visit BETWEEN '2019-06-01' and '2020-05-31'; 
+UPDATE assessments_answergroup_institution SET questiongroup_id=63 WHERE questiongroup_id=46 and date_of_visit BETWEEN '2019-06-01' and '2020-05-31'; 
+UPDATE assessments_answergroup_institution SET questiongroup_id=64 WHERE questiongroup_id=47 and date_of_visit BETWEEN '2019-06-01' and '2020-05-31'; 
