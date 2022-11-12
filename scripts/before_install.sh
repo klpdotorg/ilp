4d0675c65ed65e8fe75484726c408210c7f1198f
PaiNamratha <painamratha5@gmail.com>
2022-11-12 12:24:50 +0530

    updated before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,7 @@ WORK_DIR=/home/ubuntu/ilp
 mkdir -p $WORK_DIR
 chown -vc ubuntu:www-data $WORK_DIR || exit 1
 cd $WORK_DIR
-[[ -d venv ]] || /usr/bin/virtualenv -p /usr/bin/python venv
+[[ -d venv ]] || /usr/local/bin/virtualenv -p /usr/bin/python venv
 chown -R ubuntu:www-data venv || exit 1
 # chown -R ubuntu:www-data ~/.npm || exit 1
 chown -R ubuntu:www-data /usr/lib/node_modules || exit 1
