6af8c12d5fab7f53010e31d1870833682d6207f7
PaiNamratha <painamratha5@gmail.com>
2022-11-14 15:22:18 +0530

    updated after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,12 +2,12 @@
 WORK_DIR=/home/ubuntu/ilp
 cd $WORK_DIR
 source venv/bin/activate || exit 1
-pip install -r requirements/base.txt --exists-action w || exit 1
-pip install gunicorn || exit 1
+pip3 install -r requirements/base.txt --exists-action w || exit 1
+pip3 install gunicorn || exit 1
 cp -fv /home/ubuntu/prod_settings.py ${WORK_DIR}/ilp/settings/
-python manage.py migrate || exit 1
-python manage.py loaddata apps/*/fixtures/*.json || exit 1
-python manage.py collectstatic -c --noinput || exit 1
+python3 manage.py migrate || exit 1
+python3 manage.py loaddata apps/*/fixtures/*.json || exit 1
+python3 manage.py collectstatic -c --noinput || exit 1
 #sudo npm install -g newman || exit 1
 ##sudo npm install -g newman-reporter-html || exit 1
 
