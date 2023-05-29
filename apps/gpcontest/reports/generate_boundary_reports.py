7f35e84e984bc638ac2ffa4a7db08c56cff47133
PaiNamratha <painamratha5@gmail.com>
2023-05-29 17:44:21 +0530

    Updated Avg calculation in district and block Reports

--- apps/gpcontest/reports/generate_boundary_reports.py
+++ apps/gpcontest/reports/generate_boundary_reports.py
@@ -167,9 +167,9 @@ def generate_boundary_report(
                     overall_scores
 
                 # Find the competency scores
-                competencies = round(competency_scores.filter(
-                                    questiongroup_name=each_row["questiongroup_name"]), 2)
-                concept_scores = format_answers(each_row["questiongroup_id"], competencies)
+                competencies = competency_scores.filter(
+                                    questiongroup_name=each_row["questiongroup_name"])
+                concept_scores = format_answers(each_row["questiongroup_id"], round(competencies, 2))
                 concept_scores["total"] = each_row["total_num_students"]
                 boundary_report[each_row["questiongroup_name"]]["competency_scores"] = \
                     concept_scores
